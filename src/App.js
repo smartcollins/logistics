f4fa4cd0752a5bbdc0e3bcd296f3d0c023c4d45c
collins <kevin1520smart@gmail.com>
2023-04-16 23:05:53 +0100

    modified App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { RouterProvider,createBrowserRouter } from 'react-router-dom';
+import Footer from './component/Footer';
+import ErrorPage from './component/ErrorPage';
 
 function App() {
   const router = createBrowserRouter([
