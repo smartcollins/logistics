33808c295b14f9321ce6e68d7107ce86154b5e57
collins <kevin1520smart@gmail.com>
2023-04-17 23:39:14 +0100

    formatted Footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import color from '../data/color'
-import { Envelope,Phone, LinkedinLogo,TwitterLogo, FacebookLogo  } from 'phosphor-react';
+import { Envelope, Phone, LinkedinLogo, TwitterLogo, FacebookLogo } from 'phosphor-react';
 
 
-const Footer = ()=>{
+const Footer = () => {
     let a = color.blueBg
     // const linear = {background: linear-gradient(`94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%`)};
-    return(
-        <div style={{backgroundColor:a}} className=' capitalize text-white px-40'>
+    return (
+        <div style={{ backgroundColor: a }} className=' capitalize text-white px-40'>
             <div className=' bg-white/5 flex items-center justify-between space-x-8 font-[Rubik] font-semibold text-lg'>
-                <div style={{backgroundColor:color.blueBg}} className='w-full py-4'>
+                <div style={{ backgroundColor: color.blueBg }} className='w-full py-4'>
                     <p>TransitFlow</p>
                 </div>
                 <p className='w-2/5'>Pages</p>
@@ -17,7 +17,7 @@ const Footer = ()=>{
                 <p className='w-2/4'>Subscribe</p>
             </div>
             <div className='flex items-start justify-between space-x-8 py-4'>
-                <div style={{backgroundColor:color.blueBg}} className=' w-full normal-case space-y-4 mb-4'>
+                <div style={{ backgroundColor: color.blueBg }} className=' w-full normal-case space-y-4 mb-4'>
                     <p className='w-3/4'>Leverage agile frameworks to provide a robust synopsis for strategy  collaborative thinking to further the overall value proposition.</p>
                     <div className='flex items-center gap-4'>
                         <div className='p-3 rounded-full bg-[#111c55] border border-[#273270]'>
@@ -55,7 +55,7 @@ const Footer = ()=>{
                 <div className='w-2/4 space-y-4'>
                     <input className=' font-[League_Spartan] w-full py-2 bg-transparent border border-[#4e5683] indent-4 ' type="text" placeholder='Email here*' />
                     <div className='flex items-center justify-between'>
-                        <button style={{background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)`}} className='p-4 text-black w-2/4'>send now</button>
+                        <button style={{ background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` }} className='p-4 text-black w-2/4'>send now</button>
                         <LinkedinLogo size={20} color="#ffffff" weight="fill" />
                         <TwitterLogo size={20} color="#ffffff" weight="fill" />
                         <FacebookLogo size={20} color="#ffffff" weight="fill" />
@@ -64,11 +64,11 @@ const Footer = ()=>{
             </div>
             <div className='flex items-center justify-between border-t border-t-white py-4'>
                 <p className=' w-full'>Copyright © TransitFlow | Designed by VictorFlow - Powered by Webflow.</p>
-                <div style={{color:color.grayTxt}} className='flex items-center justify-between w-3/4'>
-                <p>Style Guide</p>
-                <p>Licenses</p>
-                <p>Changelog</p>
-                <p>Password</p>
+                <div style={{ color: color.grayTxt }} className='flex items-center justify-between w-3/4'>
+                    <p>Style Guide</p>
+                    <p>Licenses</p>
+                    <p>Changelog</p>
+                    <p>Password</p>
                 </div>
             </div>
         </div>
