e87a1013dbfa9affdcf91ae7f5b2567122aef0a2
collins <kevin1520smart@gmail.com>
2023-04-18 21:29:10 +0100

    removed some background-color in Nav

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = ()=>{
     return(
         <div>
             <div style={{ backgroundColor: color.blueBg }} className='flex items-center justify-between text-white'>
-                <div className=' py-4 flex items-center gap-4 bg-red-300 w-2/4'>
+                <div className=' py-4 flex items-center gap-4 w-2/4'>
                     <div className='h-full grid grid-cols-2 gap-1'>
                         <span className=' h-3 w-3 bg-[#ffb82b] origin-bottom rotate-[-30deg]'></span>
                         <span className=' h-3 w-3 bg-[#ffb82b]'></span>
@@ -16,8 +16,8 @@ const Nav = ()=>{
                     </div>
                     <p>TransitFlow</p>
                 </div>
-                <div className='flex items-center justify-between gap-4 bg-red-100 w-full'>
-                    <div className='flex items-center gap-4 w-full bg-red-500'>
+                <div className='flex items-center justify-between gap-4  w-full'>
+                    <div className='flex items-center gap-4 w-full '>
                         <div className='p-3 rounded-full bg-[#111c55] border border-[#273270]'>
                             <Clock size={20} color="#f6b426" />
                         </div>
@@ -26,7 +26,7 @@ const Nav = ()=>{
                             <p>Sunday Closed</p>
                         </div>
                     </div>
-                <div className='flex items-center gap-4 w-full bg-red-500'>
+                <div className='flex items-center gap-4 w-full '>
                         <div className='p-3 rounded-full bg-[#111c55] border border-[#273270]'>
                             <Envelope size={20} color="#f6b426" />
                         </div>
@@ -35,7 +35,7 @@ const Nav = ()=>{
                             <p>contact@logistics.com</p>
                         </div>
                     </div>
-                    <div className='flex items-center gap-4 w-full bg-red-500'>
+                    <div className='flex items-center gap-4 w-full '>
                         <div className='p-3 rounded-full bg-[#111c55] border border-[#273270]'>
                             <Phone size={20} color="#f6b426" />
                         </div>
