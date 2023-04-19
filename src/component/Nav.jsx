c18c6d7af69b06cdea7f1d7c35090a9431cd482d
collins <kevin1520smart@gmail.com>
2023-04-19 20:46:13 +0100

    minor change in Nav

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -5,8 +5,8 @@ import { Clock, Envelope, Phone, TwitterLogo, FacebookLogo,LinkedinLogo, Instagr
 
 const Nav = ()=>{
     return(
-        <div>
-            <div style={{ backgroundColor: color.blueBg }} className='flex items-center justify-between text-white'>
+        <div className='text-white capitalize'>
+            <div style={{ backgroundColor: color.blueBg }} className='flex items-center justify-between'>
                 <div className=' py-4 flex items-center gap-4 w-2/4'>
                     <div className='h-full grid grid-cols-2 gap-1'>
                         <span className=' h-3 w-3 bg-[#ffb82b] origin-bottom rotate-[-30deg]'></span>
@@ -46,22 +46,22 @@ const Nav = ()=>{
                     </div>
                 </div>
             </div>
-            <div className='bg-red-500 flex items-center justify-between'>
-                <div className='flex items-center justify-between bg-red-200 w-2/4'>
-                    <p>home</p>
-                    <p>about</p>
-                    <p>pages</p>
-                    <p>projects</p>
-                    <p>contact</p>
+            <div className=' bg-[#091242]/5 font-medium flex items-center justify-between'>
+                <div className='flex items-center bg-red-200 w-2/4 text-center'>
+                    <p className='border-r h-full py-4 last:border-none border-r-white w-full'>home</p>
+                    <p className='border-r h-full py-4 last:border-none border-r-white w-full'>about</p>
+                    <p className='border-r h-full py-4 last:border-none border-r-white w-full'>pages</p>
+                    <p className='border-r h-full py-4 last:border-none border-r-white w-full'>projects</p>
+                    <p className='border-r h-full py-4 last:border-none border-r-white w-full'>contact</p>
                 </div>
                 <div className='flex items-center justify-between w-2/5'>
                     <div className='flex items-center justify-between w-2/5'>
-                        <InstagramLogo size={20} color="#ffffff" weight="fill" />
+                        <InstagramLogo size={20} color="#ffffff" />
                         <LinkedinLogo size={20} color="#ffffff" weight="fill" />
                         <TwitterLogo size={20} color="#ffffff" weight="fill" />
                         <FacebookLogo size={20} color="#ffffff" weight="fill" />
                     </div>
-                    <button className='p-4 text-black bg-white w-2/4'>request quote</button>
+                    <button className='p-4 text-black bg-white w-2/4 capitalize'>request quote</button>
                 </div>
             </div>
         </div>
