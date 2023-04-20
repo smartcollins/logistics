e627eefa1b370756d7a963ce97702dcabe311088
collins <kevin1520smart@gmail.com>
2023-04-20 13:32:39 +0100

    created Banner

---
+++ src/component/Banner.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+const Banner = ()=>{
+    return(
+        <div className='bg-red-500'>
+            <div>
+            <p>Logistics & Supply Chain Solutions</p>
+            <p>Your Gateway to any Destination in the World</p>
+            <p>In augue ligula, feugiat ut nulla consequat. Ut est lacus, molestie in arcu no, iaculis vehicula ipsum. Nunc faucibus, nisl id dapibus finibus, enim diam interdum nulla, sed laoreet risus lectus. </p>
+            <button>explore more</button>
+            </div>
+        </div>
+    )
+}
+
+
+export default Banner; 
