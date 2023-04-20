ccd29e4193ff34f5f2056b26f10c0c9312024c19
collins <kevin1520smart@gmail.com>
2023-04-20 20:23:28 +0100

    modified button tag in Banner

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -11,7 +11,7 @@ const Banner = ()=>{
             <p style={{borderLeft:`2px solid ${color.grdt}`}} className='bg-[#041c37]/50 w-fit px-2'>Logistics & Supply Chain Solutions</p>
             <p className=' w-[55%] font-[Rubik] font-semibold text-5xl leading-normal'>Your Gateway to any Destination in the World</p>
             <p className=' font-[Krub] font-medium w-3/5'>In augue ligula, feugiat ut nulla consequat. Ut est lacus, molestie in arcu no, iaculis vehicula ipsum. Nunc faucibus, nisl id dapibus finibus, enim diam interdum nulla, sed laoreet risus lectus. </p>
-            <button>explore more</button>
+            <button style={{ background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` }} className='p-4 text-black capitalize font-medium my-4'>explore more</button>
             </div>
         </div>
     )
