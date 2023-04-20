0cb6cb98dfa10fac8de4f7bf2403da6699d5c088
collins <kevin1520smart@gmail.com>
2023-04-20 20:19:27 +0100

    modified last p tag in Banner

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = ()=>{
             {/* linear-gradient(94.06deg, #FFB629 -1.21%, #FFDA56 58.66%, #FFD7A6 116.84%) */}
             <p style={{borderLeft:`2px solid ${color.grdt}`}} className='bg-[#041c37]/50 w-fit px-2'>Logistics & Supply Chain Solutions</p>
             <p className=' w-[55%] font-[Rubik] font-semibold text-5xl leading-normal'>Your Gateway to any Destination in the World</p>
-            <p>In augue ligula, feugiat ut nulla consequat. Ut est lacus, molestie in arcu no, iaculis vehicula ipsum. Nunc faucibus, nisl id dapibus finibus, enim diam interdum nulla, sed laoreet risus lectus. </p>
+            <p className=' font-[Krub] font-medium w-3/5'>In augue ligula, feugiat ut nulla consequat. Ut est lacus, molestie in arcu no, iaculis vehicula ipsum. Nunc faucibus, nisl id dapibus finibus, enim diam interdum nulla, sed laoreet risus lectus. </p>
             <button>explore more</button>
             </div>
         </div>
