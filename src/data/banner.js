a00068cc2cac83ab3e350f0d23711325b413ace9
collins <kevin1520smart@gmail.com>
2023-04-21 14:55:51 +0100

    created banner.js

---
+++ src/data/banner.js
@@ -0,0 +1,6 @@
+const banner = [{
+    txt: 'Logistics & Supply Chain Solutions',
+    info: 'Your Gateway to any Destination in the World',
+    txt2: 'In augue ligula, feugiat ut nulla consequat. Ut est lacus, molestie in arcu no, iaculis vehicula ipsum. Nunc faucibus, nisl id dapibus finibus, enim diam interdum nulla, sed laoreet risus lectus.',
+    btnTxt: 'explore more',
+}]
