739933cab01632a98a8f02b791d01e5d94b82c29
collins <kevin1520smart@gmail.com>
2023-04-22 16:21:01 +0100

    modifying Banner

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import home from '../images/br-home.png'
 import color from '../data/color';
+import { TwitterLogo, FacebookLogo, LinkedinLogo, InstagramLogo } from 'phosphor-react';
 
 const Banner = ({img,txt,txt2,extra,info,btn_txt}) => {
     return (
@@ -13,13 +13,37 @@ const Banner = ({img,txt,txt2,extra,info,btn_txt}) => {
         //         <button style={{ background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` }} className='p-4 text-black capitalize font-medium my-4'>explore more</button>
         //     </div>
         // </div>
-        <div className='bg-red-400 bg-no-repeat bg-cover bg-center relative h-[70vh]' style={{ backgroundImage: `url(${img})` }}>
-            <div className=' absolute top-2/4 -translate-y-2/4'>
+        // <div style={{ backgroundImage: `url(${img})` }} className='bg-no-repeat bg-cover bg-center relative h-[70vh]'>
+        <div style={{ backgroundImage: `url(${img})` }} className='bg-no-repeat bg-cover bg-center relative h-screen'>
+            <div className=' bg-[#091242]/5 font-medium flex items-center justify-between'>
+                <div className='flex items-center w-2/4 text-center'>
+                    <p className='border-r last:border-none border-r-white w-full'>home</p>
+                    <p className='border-r last:border-none border-r-white w-full'>about</p>
+                    <p className='border-r last:border-none border-r-white w-full'>pages</p>
+                    <p className='border-r last:border-none border-r-white w-full'>projects</p>
+                    <p className='border-r last:border-none border-r-white w-full'>contact</p>
+                </div>
+                <div className='flex items-center justify-between w-2/5'>
+                    <div className='flex items-center justify-between w-2/5'>
+                        <InstagramLogo size={20} color="#ffffff" />
+                        <LinkedinLogo size={20} color="#ffffff" weight="fill" />
+                        <TwitterLogo size={20} color="#ffffff" weight="fill" />
+                        <FacebookLogo size={20} color="#ffffff" weight="fill" />
+                    </div>
+                    <button className='p-4 text-black bg-white w-2/4 capitalize'>request quote</button>
+                </div>
+            </div>
+            <div className=' absolute top-1/2 -translate-y-1/2 w-2/4'>
                 {/* linear-gradient(94.06deg, #FFB629 -1.21%, #FFDA56 58.66%, #FFD7A6 116.84%) */}
                 <p style={{ borderLeft: `2px solid ${color.grdt}` }} className='bg-[#041c37]/50 w-fit px-2 py-1'>{txt}</p>
-                <p className=' w-[25%] font-[Rubik] font-semibold text-4xl/normal'>{info}</p>
-                <p className=' font-[Krub] font-medium w-[35%]'>{txt2}</p>
-                <button style={{ background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` }} className='p-4 text-black capitalize font-medium my-4'>{btn_txt}</button>
+                {/* <p className=' w-[25%] font-[Rubik] font-semibold text-4xl/normal'>{info}</p> */}
+                <p className='font-[Rubik] font-semibold text-4xl/normal w-[50%]'>{info}</p>
+                {extra&&
+                    <div>
+                        <p className=' font-[Krub] font-medium w-3/5 my-2 '>{txt2}</p>
+                        <button style={{ background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` }} className='p-2 text-black capitalize font-medium'>{btn_txt}</button>
+                    </div>
+                }
             </div>
         </div>
     )
