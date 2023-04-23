fe4b1c7cb8612c253d2958a5924429d418ee7ad7
collins <kevin1520smart@gmail.com>
2023-04-23 23:40:11 +0100

    formatted Banner

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -2,39 +2,39 @@ import React from 'react';
 import color from '../data/color';
 import { TwitterLogo, FacebookLogo, LinkedinLogo, InstagramLogo } from 'phosphor-react';
 
-const Banner = ({img,txt,txt2,extra,info,btn_txt}) => {
+const Banner = ({ img, txt, txt2, extra, info, btn_txt }) => {
     return (
         <div className='h-screen'>
             <div style={{ backgroundImage: `url(${img})` }} className='bg-no-repeat bg-cover bg-center h-4/5 relative'>
-            <div className=' bg-[#091242]/5 font-medium flex items-center justify-between'>
-                <div className='flex items-center w-2/4 text-center'>
-                    <p className='border-r last:border-none border-r-white w-full'>home</p>
-                    <p className='border-r last:border-none border-r-white w-full'>about</p>
-                    <p className='border-r last:border-none border-r-white w-full'>pages</p>
-                    <p className='border-r last:border-none border-r-white w-full'>projects</p>
-                    <p className='border-r last:border-none border-r-white w-full'>contact</p>
-                </div>
-                <div className='flex items-center justify-between w-2/5'>
+                <div className=' bg-[#091242]/5 font-medium flex items-center justify-between'>
+                    <div className='flex items-center w-2/4 text-center'>
+                        <p className='border-r last:border-none border-r-white w-full'>home</p>
+                        <p className='border-r last:border-none border-r-white w-full'>about</p>
+                        <p className='border-r last:border-none border-r-white w-full'>pages</p>
+                        <p className='border-r last:border-none border-r-white w-full'>projects</p>
+                        <p className='border-r last:border-none border-r-white w-full'>contact</p>
+                    </div>
                     <div className='flex items-center justify-between w-2/5'>
-                        <InstagramLogo size={20} color="#ffffff" />
-                        <LinkedinLogo size={20} color="#ffffff" weight="fill" />
-                        <TwitterLogo size={20} color="#ffffff" weight="fill" />
-                        <FacebookLogo size={20} color="#ffffff" weight="fill" />
+                        <div className='flex items-center justify-between w-2/5'>
+                            <InstagramLogo size={20} color="#ffffff" />
+                            <LinkedinLogo size={20} color="#ffffff" weight="fill" />
+                            <TwitterLogo size={20} color="#ffffff" weight="fill" />
+                            <FacebookLogo size={20} color="#ffffff" weight="fill" />
+                        </div>
+                        <button className='p-4 text-black bg-white w-2/4 capitalize'>request quote</button>
                     </div>
-                    <button className='p-4 text-black bg-white w-2/4 capitalize'>request quote</button>
+                </div>
+                <div className=' absolute top-1/2 -translate-y-1/2 w-2/4'>
+                    <p style={{ borderLeft: `2px solid ${color.grdt}` }} className='bg-[#041c37]/50 w-fit px-2 py-1'>{txt}</p>
+                    <p className='font-[Rubik] font-semibold text-4xl/normal w-[50%]'>{info}</p>
+                    {extra &&
+                        <div>
+                            <p className=' font-[Krub] font-medium w-3/5 my-2 '>{txt2}</p>
+                            <button style={{ background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` }} className='p-2 text-black capitalize font-medium'>{btn_txt}</button>
+                        </div>
+                    }
                 </div>
             </div>
-            <div className=' absolute top-1/2 -translate-y-1/2 w-2/4'>
-                <p style={{ borderLeft: `2px solid ${color.grdt}` }} className='bg-[#041c37]/50 w-fit px-2 py-1'>{txt}</p>
-                <p className='font-[Rubik] font-semibold text-4xl/normal w-[50%]'>{info}</p>
-                {extra&&
-                    <div>
-                        <p className=' font-[Krub] font-medium w-3/5 my-2 '>{txt2}</p>
-                        <button style={{ background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` }} className='p-2 text-black capitalize font-medium'>{btn_txt}</button>
-                    </div>
-                }
-            </div>
-        </div>
         </div>
     )
 }
