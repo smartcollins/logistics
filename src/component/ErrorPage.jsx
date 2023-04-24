9fcadb19ed1c4d05085964e13a6e03e04fbdad88
collins <kevin1520smart@gmail.com>
2023-04-24 23:51:14 +0100

    styled button tag in ErrorPage

--- src/component/ErrorPage.jsx
+++ src/component/ErrorPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import banner from '../data/banner';
 import Banner from './Banner';
 import Footer from './Footer';
+import color from '../data/color';
 
 
 const ErrorPage = ()=>{
@@ -12,7 +13,7 @@ const ErrorPage = ()=>{
                 <p className='text-[200px] font-semibold text-[#1c1f35]'>4<span className='text-[#ffbb2b]'>0</span>4</p>
                 <p className='text-[#1c1f35] font-semibold text-2xl -mt-10'>Oops! Page not found.</p>
                 <p className='text-[#666c89] text-base'>Lets's get you to where you need to be.</p>
-                <button>Back to home</button>
+                <button style={{ background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` }} className='px-4 py-2 my-4 font-medium text-black'>Back to home</button>
             </div>
             <Footer/>
         </div>
