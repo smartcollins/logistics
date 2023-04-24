5a0762b93d432087f33c58ad5cc37ce5cb21e55b
collins <kevin1520smart@gmail.com>
2023-04-24 23:34:35 +0100

    increased font size in ErrorPage

--- src/component/ErrorPage.jsx
+++ src/component/ErrorPage.jsx
@@ -9,7 +9,7 @@ const ErrorPage = ()=>{
         <div>
             <div>{banner.slice(11,12).map((item, idx) => <Banner key={idx} {...item} />)}</div>
             <div className='h-[70vh] text-center'>
-                <p className='text-9xl font-semibold text-[#1c1f35]'>4<span className='text-[#ffbb2b]'>0</span>4</p>
+                <p className='text-[200px] font-semibold text-[#1c1f35]'>4<span className='text-[#ffbb2b]'>0</span>4</p>
                 <p>Oops! Page not found</p>
                 <p>Lets's get you to where you need to be.</p>
                 <button>Back to home</button>
