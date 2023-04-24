230d568e45396e92d602e4315f77132adfb649f3
collins <kevin1520smart@gmail.com>
2023-04-24 23:21:12 +0100

    workinf on ErrorPage

--- src/component/ErrorPage.jsx
+++ src/component/ErrorPage.jsx
@@ -6,7 +6,13 @@ import Banner from './Banner';
 const ErrorPage = ()=>{
     return(
         <div>
-            {banner.slice(11,12).map((item, idx) => <Banner key={idx} {...item} />)}
+            <div>{banner.slice(11,12).map((item, idx) => <Banner key={idx} {...item} />)}</div>
+            <div>
+                <p>4<span>0</span>4</p>
+                <p>Oops! Page not found</p>
+                <p>Lets's get you to where you need to be.</p>
+                <button>Back to home</button>
+            </div>
         </div>
     )
 }
