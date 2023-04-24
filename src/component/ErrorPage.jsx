40aa11f4e84e607dc36d408018afe70172c41691
collins <kevin1520smart@gmail.com>
2023-04-24 23:44:50 +0100

    work in progress

--- src/component/ErrorPage.jsx
+++ src/component/ErrorPage.jsx
@@ -10,8 +10,8 @@ const ErrorPage = ()=>{
             <div>{banner.slice(11,12).map((item, idx) => <Banner key={idx} {...item} />)}</div>
             <div className='h-[70vh] text-center'>
                 <p className='text-[200px] font-semibold text-[#1c1f35]'>4<span className='text-[#ffbb2b]'>0</span>4</p>
-                <p className='text-[#1c1f35] font-semibold text-2xl'>Oops! Page not found.</p>
-                <p>Lets's get you to where you need to be.</p>
+                <p className='text-[#1c1f35] font-semibold text-2xl -mt-10'>Oops! Page not found.</p>
+                <p className='text-[#666c89] text-base'>Lets's get you to where you need to be.</p>
                 <button>Back to home</button>
             </div>
             <Footer/>
