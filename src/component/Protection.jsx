6b692d287ba2300af4e7022813d9fccdb8304166
collins <kevin1520smart@gmail.com>
2023-04-25 16:21:03 +0100

    modifying Protection

--- src/component/Protection.jsx
+++ src/component/Protection.jsx
@@ -9,17 +9,17 @@ const Protection = ()=>{
     return(
         <div>
             <div>{banner.slice(14, 15).map((item, idx) => <Banner key={idx} {...item} />)}</div>
-            <div style={{backgroundColor: color.blueBg}} className='h-[30vh] my-8 mx-auto w-2/4 capitalize text-center flex flex-col items-center'>
-                <div className='bg-red-500 m-auto w-2/5 h-2/4 flex flex-col items-center justify-between'>
+            <div style={{backgroundColor: color.blueBg}} className='h-[45vh] my-8 mx-auto w-2/4 capitalize text-center flex flex-col items-center'>
+                <div className='m-auto w-2/4 h-2/4 flex flex-col items-center justify-between'>
                     <p className='text-white text-2xl font-bold'>enter password</p>
-                    <div className=' bg-red-200 w-full flex gap-4'>
-                        <input className=' indent-4 placeholder:capitalize caret-[#999999] py-2 border border-[#4e5683] outline-[#4e5683]' type="text" placeholder='enter your password' />
+                    <div className=' w-full flex items-center gap-4'>
+                        <input className=' bg-transparent text-white indent-4 placeholder:capitalize caret-[#999999] py-2 border border-[#4e5683] outline-[#4e5683]' type="text" placeholder='enter your password' />
                         <button style={{
                     background: `linear-gradient(94.06deg,
                                 ${color.grdt} -1.21%, ${color.grdt2} 58.66%,
                                 ${color.grdt3} 116.84%)`
                 }}
-                    className='w-full font-medium text-base'>submit</button>
+                    className='w-full font-medium text-base py-2'>submit</button>
                     </div>
                 </div>
             </div>
