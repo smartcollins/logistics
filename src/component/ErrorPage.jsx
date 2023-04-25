cb9d6f49b0ae94e0ca1daf0e26de1fc8abc4d81c
collins <kevin1520smart@gmail.com>
2023-04-25 15:11:21 +0100

    minor modification in ErrorPage

--- src/component/ErrorPage.jsx
+++ src/component/ErrorPage.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import color from '../data/color';
 import banner from '../data/banner';
 import Banner from './Banner';
 import Footer from './Footer';
-import color from '../data/color';
 
 
-const ErrorPage = ()=>{
-    return(
+const ErrorPage = () => {
+    return (
         <div>
-            <div>{banner.slice(11,12).map((item, idx) => <Banner key={idx} {...item} />)}</div>
-            <div className='h-[70vh] text-center'>
-                <p className='text-[200px] font-semibold text-[#1c1f35]'>4<span className='text-[#ffbb2b]'>0</span>4</p>
-                <p className='text-[#1c1f35] font-bold text-2xl -mt-10 '>Oops! Page not found.</p>
-                <p className='text-[#666c89] text-base tracking-tighter '>Lets's get you to where you need to be.</p>
-                <button style={{ background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` }} className='px-4 py-2 my-4 font-medium text-black'>Back to home</button>
+            <div>{banner.slice(11, 12).map((item, idx) => <Banner key={idx} {...item} />)}</div>
+            <div className='h-[70vh] text-center text-[#1c1f35]'>
+                <p className='text-[200px] font-semibold'>4<span className='text-[#ffbb2b]'>0</span>4</p>
+                <p className='font-bold text-2xl -mt-10'>Oops! Page not found.</p>
+                <p className='text-[#666c89] text-base tracking-tighter'>Lets's get you to where you need to be.</p>
+                <button style={{
+                    background: `linear-gradient(94.06deg,
+                                ${color.grdt} -1.21%, ${color.grdt2} 58.66%,
+                                ${color.grdt3} 116.84%)`
+                }}
+                    className='px-4 py-2 my-4 font-medium text-black'>Back to home</button>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
