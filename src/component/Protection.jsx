3bacbac41e22e0c1382a9b9db2a5a810a88ceca2
collins <kevin1520smart@gmail.com>
2023-04-25 15:28:09 +0100

    modifying Protection

--- src/component/Protection.jsx
+++ src/component/Protection.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import color from '../data/color';
+import banner from '../data/banner';
+import Banner from './Banner';
 
 
 const Protection = ()=>{
-    <div></div>
+    <div>
+        <div>{banner.slice(11, 12).map((item, idx) => <Banner key={idx} {...item} />)}</div>
+    </div>
 }
 
 
