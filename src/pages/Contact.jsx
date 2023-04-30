991288c0da11639999aebfabd5bc44c1cfc803b7
collins <kevin1520smart@gmail.com>
2023-04-30 20:54:35 +0100

    added Banner to Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import banner from '../data/banner';
+import Banner from '../component/Banner';
 
 
 const Contact = ()=>{
     return(
-        <div></div>
+        <div>
+            <div>{banner.slice(9, 10).map((item, idx) => <Banner key={idx} {...item} />)}</div>
+        </div>
     )
 }
 
