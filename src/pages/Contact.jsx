8fca7fe13acec634167966dc459aed9970a0ec15
collins <kevin1520smart@gmail.com>
2023-04-30 22:10:27 +0100

    modifying Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = ()=>{
     return(
         <div className=' capitalize text-white'>
             <div>{banner.slice(9, 10).map((item, idx) => <Banner key={idx} {...item} />)}</div>
-            <div style={{background: color.blueBg}} className=' w-4/5 mx-auto my-8 py-8'>
+            <div style={{background: color.blueBg}} className=' w-4/5 mx-auto my-8 py-8 px-16'>
                 <div className='mx-auto w-fit text-center'>
                     <p style={{ borderLeft: `4px solid ${color.grdt}` }} className=' bg-[#e8e8e8]/10 w-fit px-4 mx-auto'>contact</p>
                     <p className='font-[Rubik] font-bold text-4xl/normal'>get in touch with us</p>
@@ -19,13 +19,14 @@ const Contact = ()=>{
                     <Phone className='w-16 h-16 bg-[#273270] border border-[#273270] rounded-full px-4' size={30} color="#ffffff" />
                     <Clock className='w-16 h-16 bg-[#273270] border border-[#273270] rounded-full px-4' size={30} color="#ffffff" />
                 </div>
-                <div className=' grid grid-rows-4 grid-cols-2 gap-4'>
-                    <input type="text" className='bg-red-300 h-16 w-full' />
-                    <input type="text" className='bg-red-300 h-16 w-full' />
-                    <input type="text" className='bg-red-300 h-16 w-full' />
-                    <input type="text" className='bg-red-300 h-16 w-full' />
+                <div className=' grid grid-rows-4 grid-cols-2 gap-4 font-["League_Spartan"] capitalize text-4xl'>
+                    <input type="text" placeholder='Your name*' className=' indent-4 text-2xl placeholder:text-white placeholder:text-2xl bg-transparent border border-white h-16 w-full' />
+                    <input type="text" placeholder='Email*' className=' indent-4 text-2xl placeholder:text-white placeholder:text-2xl bg-transparent border border-white h-16 w-full' />
+                    <input type="number" placeholder='Phone Number*' className=' indent-4 text-2xl placeholder:text-white placeholder:text-2xl bg-transparent border border-white h-16 w-full' />
+                    <input type="text" placeholder='City' className=' indent-4 text-2xl placeholder:text-white placeholder:text-2xl bg-transparent border border-white h-16 w-full' />
+                    <textarea placeholder='Your Message' className=' indent-4 text-2xl placeholder:text-white placeholder:text-2xl bg-transparent border border-white w-full col-span-2 row-span-2' name="" id=""></textarea>
+                    {/* <textarea placeholder='Your Message' className=' indent-4 text-2xl placeholder:text-white placeholder:text-2xl bg-transparent border border-white w-full col-span-2 row-span-2' name="" id="" cols="30" rows="10"></textarea> */}
                     {/* <input className='bg-red-300 w-full col-span-2 row-span-2'/> */}
-                    <textarea className='bg-red-300 w-full col-span-2 row-span-2' name="" id="" cols="30" rows="10"></textarea>
                 </div>
                 <button>send message</button>
             </div>
