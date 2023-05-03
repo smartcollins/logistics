f8ff1a5d743df757ecfeaa0a6a363631e48be158
collins <kevin1520smart@gmail.com>
2023-05-03 21:54:21 +0100

    made use of Caption in Blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import banner from '../data/banner';
 import color from '../data/color';
 import Banner from '../component/Banner';
+import Caption from '../component/Caption';
 
 
 const Blog = ()=>{
@@ -11,6 +12,13 @@ const Blog = ()=>{
             <div className='my-4 capitalize'>
                 <p style={{ borderLeft: `4px solid ${color.grdt}` }} className=' bg-[#e8e8e8]/10 w-fit px-4 mx-auto font-semibold'>our blog</p>
                 <p className='font-[Rubik] font-bold text-4xl/normal mx-auto w-fit'>our latest news</p>
+            </div>
+            <div className='bg-red-500 mx-auto w-fit'>
+            <Caption
+                dark={true}
+                txt={'our blog'}
+                txt2={'our latest news'}
+            />
             </div>
         </div>
     )
