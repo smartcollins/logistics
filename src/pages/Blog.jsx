c8b3f1cd10cf043d36316a0f97a9d44d538681ae
collins <kevin1520smart@gmail.com>
2023-05-03 22:05:14 +0100

    minor modification in Blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -9,11 +9,11 @@ const Blog = ()=>{
     return(
         <div>
             <div>{banner.slice(6, 7).map((item, idx) => <Banner key={idx} {...item} />)}</div>
-            <div className='my-4 capitalize'>
+            {/* <div className='my-4 capitalize'>
                 <p style={{ borderLeft: `4px solid ${color.grdt}` }} className=' bg-[#e8e8e8]/10 w-fit px-4 mx-auto font-semibold'>our blog</p>
                 <p className='font-[Rubik] font-bold text-4xl/normal mx-auto w-fit'>our latest news</p>
-            </div>
-            <div className='bg-red-500 mx-auto w-fit'>
+            </div> */}
+            <div className=' mx-auto w-fit'>
             <Caption
                 dark={true}
                 txt={'our blog'}
