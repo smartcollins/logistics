77d565a8d00a5b07edcd53340731906ce2020d6a
collins <kevin1520smart@gmail.com>
2023-05-03 20:58:52 +0100

    modifying Caption

--- src/component/Caption.jsx
+++ src/component/Caption.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 
 const Caption = ()=>{
     return(
-        <div></div>
+        <div className='my-4 capitalize'>
+            <p style={{ borderLeft: `4px solid ${color.grdt}` }} className=' bg-[#e8e8e8]/10 w-fit px-4 mx-auto font-semibold'>our blog</p>
+            <p className='font-[Rubik] font-bold text-4xl/normal mx-auto w-fit'>our latest news</p>
+        </div>
     )
 }
 
