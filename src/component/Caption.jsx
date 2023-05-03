4ffe1008f68b0cfa6ac8abfea9f10df01c864874
collins <kevin1520smart@gmail.com>
2023-05-03 21:52:49 +0100

    modified Caption

--- src/component/Caption.jsx
+++ src/component/Caption.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import color from '../data/color';
 
 
-const Caption = ()=>{
+const Caption = ({dark,txt,txt2})=>{
     return(
         <div className='my-4 capitalize'>
-            <p style={{ borderLeft: `4px solid ${color.grdt}` }} className=' bg-[#e8e8e8]/10 w-fit px-4 mx-auto font-semibold'>our blog</p>
-            <p className='font-[Rubik] font-bold text-4xl/normal mx-auto w-fit'>our latest news</p>
+            {/* <p style={dark?{background: 'rgb(232 232 232 / 0.1)'}:light?{background: 'rgb(232 232 232 / 0.5)'}:left?{margin: '50'}:{margin: 'auto'}} className='w-fit px-4 font-semibold border-l-4 border-l-[#ffb629]'>{txt}</p> */}
+            <p style={dark?{background: 'rgb(232 232 232 / 0.5)'}:{background: 'rgb(232 232 232 / 0.1)'}} className=' px-4 font-semibold border-l-4 border-l-[#ffb629]'>{txt}</p>
+            <p className='font-[Rubik] font-bold text-4xl/normal'>{txt2}</p>
         </div>
     )
 }
