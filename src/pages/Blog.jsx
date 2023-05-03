0dfdeb35db39dd05e16cd28073c295e26ab33923
collins <kevin1520smart@gmail.com>
2023-05-03 20:57:18 +0100

    work in progress

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import banner from '../data/banner';
+import color from '../data/color';
 import Banner from '../component/Banner';
 
 
 const Blog = ()=>{
     return(
         <div>
-            <div>{banner.slice(7, 8).map((item, idx) => <Banner key={idx} {...item} />)}</div>
+            <div>{banner.slice(6, 7).map((item, idx) => <Banner key={idx} {...item} />)}</div>
+            <div className='my-4 capitalize'>
+                <p style={{ borderLeft: `4px solid ${color.grdt}` }} className=' bg-[#e8e8e8]/10 w-fit px-4 mx-auto font-semibold'>our blog</p>
+                <p className='font-[Rubik] font-bold text-4xl/normal mx-auto w-fit'>our latest news</p>
+            </div>
         </div>
     )
 }
