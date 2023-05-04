4cfc3a3449c611402851cef2dcc2fff8b6de6911
collins <kevin1520smart@gmail.com>
2023-05-04 19:00:05 +0100

    created News

---
+++ src/component/News.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ccc from '../images/br-blog2.png'
+
+
+const News = ()=>{
+    return(
+        <div>
+            <div>
+            <img src={ccc} alt="news" />
+            </div>
+            <div>
+                <p>08</p>
+                <p>march</p>
+            </div>
+            <div>
+                <p>Inland freight a worthy solution for your business</p>
+                <p>We are dedicated in creating added value for our customers by implementing modern technology in our work. </p>
+                <ul>
+                    <li>urgent transport solutions</li>
+                    <li>reliable & experienced staffs</li>
+                    <li>urgent transport solutions</li>
+                    <li>reliable & experienced staffs</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+
+export default News; 
