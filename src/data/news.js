d7cdb51187df623dbb696de5ae38a8a74658b7c1
collins <kevin1520smart@gmail.com>
2023-05-05 23:12:04 +0100

    formatted News and news.js

--- src/data/news.js
+++ src/data/news.js
@@ -6,65 +6,65 @@ import img5 from '../images/news5.png'
 
 const news = [
     {
-      img: img,
-      date: "08",
-      info: "inland frieght a worthy solution for your business",
-      txt: "we are dedicated in creating added value for our customers by implementing modern technology in our work",
-      list: [
-        "Urgent transport solutions",
-        "Reliable & experienced staffs",
-        "Real time tracking",
-        "Custom duty",
-      ],
+        img: img,
+        date: "08",
+        info: "inland frieght a worthy solution for your business",
+        txt: "we are dedicated in creating added value for our customers by implementing modern technology in our work",
+        list: [
+            "Urgent transport solutions",
+            "Reliable & experienced staffs",
+            "Real time tracking",
+            "Custom duty",
+        ],
     },
     {
-      img: img2,
-      date: "12",
-      info: "how technology can help redraw the supply chain map",
-      txt: "we are dedicated in creating added value for our customers by implementing modern technology in our work",
-      list: [
-        "Urgent transport solutions",
-        "Reliable & experienced staffs",
-        "Real time tracking",
-        "Custom duty",
-      ],
+        img: img2,
+        date: "12",
+        info: "how technology can help redraw the supply chain map",
+        txt: "we are dedicated in creating added value for our customers by implementing modern technology in our work",
+        list: [
+            "Urgent transport solutions",
+            "Reliable & experienced staffs",
+            "Real time tracking",
+            "Custom duty",
+        ],
     },
     {
-      img: img3,
-      date: "25",
-      info: "five things you should have ready for your broker",
-      txt: "we are dedicated in creating added value for our customers by implementing modern technology in our work",
-      list: [
-        "Urgent transport solutions",
-        "Reliable & experienced staffs",
-        "Real time tracking",
-        "Custom duty",
-      ],
+        img: img3,
+        date: "25",
+        info: "five things you should have ready for your broker",
+        txt: "we are dedicated in creating added value for our customers by implementing modern technology in our work",
+        list: [
+            "Urgent transport solutions",
+            "Reliable & experienced staffs",
+            "Real time tracking",
+            "Custom duty",
+        ],
     },
     {
-      img: img4,
-      date: "28",
-      info: "four simple tips for becoming a healthier truck driver",
-      txt: "we are dedicated in creating added value for our customers by implementing modern technology in our work",
-      list: [
-        "Urgent transport solutions",
-        "Reliable & experienced staffs",
-        "Real time tracking",
-        "Custom duty",
-      ],
+        img: img4,
+        date: "28",
+        info: "four simple tips for becoming a healthier truck driver",
+        txt: "we are dedicated in creating added value for our customers by implementing modern technology in our work",
+        list: [
+            "Urgent transport solutions",
+            "Reliable & experienced staffs",
+            "Real time tracking",
+            "Custom duty",
+        ],
     },
     {
-      img: img5,
-      date: "08",
-      info: "what is the role of a logistics operations manager",
-      txt: "we are dedicated in creating added value for our customers by implementing modern technology in our work",
-      list: [
-        "Urgent transport solutions",
-        "Reliable & experienced staffs",
-        "Real time tracking",
-        "Custom duty",
-      ],
+        img: img5,
+        date: "08",
+        info: "what is the role of a logistics operations manager",
+        txt: "we are dedicated in creating added value for our customers by implementing modern technology in our work",
+        list: [
+            "Urgent transport solutions",
+            "Reliable & experienced staffs",
+            "Real time tracking",
+            "Custom duty",
+        ],
     }
-  ];
+];
 
 export default news;
