743b6720e2b78165ecad842e22858f73550bc2fd
collins <kevin1520smart@gmail.com>
2023-05-05 22:50:40 +0100

    modifying news.js

--- src/data/news.js
+++ src/data/news.js
@@ -1,6 +1,12 @@
+import img from '../images/news.png'
+import img2 from '../images/news2.png'
+import img3 from '../images/news3.png'
+import img4 from '../images/news4.png'
+import img5 from '../images/news5.png'
+
 const news = [
     {
-      img: ccc,
+      img: img,
       date: "08",
       info: "inland frieght a worthy solution for your business",
       txt: "we are dedicated in creating added value for our customers by implementing modern technology in our work",
