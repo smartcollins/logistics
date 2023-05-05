9ff58fcc4282e1fd06a43146043e7f50cc3506bb
collins <kevin1520smart@gmail.com>
2023-05-05 23:05:01 +0100

    modified Blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import banner from '../data/banner';
-// import color from '../data/color';
+import news from '../data/news';
 import Banner from '../component/Banner';
 import Caption from '../component/Caption';
 import News from '../component/News';
 
 
 const Blog = () => {
+    const items  = news.map((item,idx)=><News key={idx} {...item}/>)
     return (
         <div>
             <div>{banner.slice(6, 7).map((item, idx) => <Banner key={idx} {...item} />)}</div>
             <Caption mid={true} dark={true} txt={'our blog'} txt2={'our latest news'}/>
-            <News/>
+            {items}
         </div>
     )
 }
