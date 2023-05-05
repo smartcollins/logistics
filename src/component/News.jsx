ca3797e5eeb9f3aff870e3540ec3e25fb129b2df
collins <kevin1520smart@gmail.com>
2023-05-05 21:34:43 +0100

    removed background colors

--- src/component/News.jsx
+++ src/component/News.jsx
@@ -5,18 +5,18 @@ import color from '../data/color'
 
 const News = ()=>{
     return(
-        <div className='flex bg-red-500 gap-4'>
-            <div className='bg-red-400 w-3/4'>
+        <div className='flex  gap-4 border-y py-4'>
+            <div className=' w-3/4'>
                 <img className='h-full object-cover' src={ccc} alt="news" />
             </div>
-            <div className='bg-red-300 text-center'>
+            <div className=' text-center'>
                 <p style={{color: color.darkTxt}} className=' font-bold text-2xl'>08</p>
                 <p style={{color: color.grayTxt}} className=' capitalize text-sm'>march</p>
             </div>
-            <div className=' font-["Rubik"] bg-red-200 border-l border-[#d6d6d6] px-2'>
+            <div className=' font-["Rubik"]  border-l border-[#d6d6d6] px-4'>
                 <p className=' font-medium'>Inland freight a worthy solution for your business</p>
                 <p className=' text-[#666c89]'>We are dedicated in creating added value for our customers by implementing modern technology in our work. </p>
-                <ul className=' list-inside list-disc bg-red-400'>
+                <ul className=' list-inside list-disc '>
                     <li>urgent transport solutions</li>
                     <li>reliable & experienced staffs</li>
                     <li>urgent transport solutions</li>
