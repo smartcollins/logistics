fadc0c1f295435b5e449227d8ce1560d2098ab45
collins <kevin1520smart@gmail.com>
2023-05-05 22:24:36 +0100

    made us of News in Blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import banner from '../data/banner';
-import color from '../data/color';
+// import color from '../data/color';
 import Banner from '../component/Banner';
 import Caption from '../component/Caption';
+import News from '../component/News';
 
 
 const Blog = () => {
     return (
         <div>
             <div>{banner.slice(6, 7).map((item, idx) => <Banner key={idx} {...item} />)}</div>
-                <Caption mid={true} dark={true} txt={'our blog'} txt2={'our latest news'}/>
+            <Caption mid={true} dark={true} txt={'our blog'} txt2={'our latest news'}/>
+            <News/>
         </div>
     )
 }
