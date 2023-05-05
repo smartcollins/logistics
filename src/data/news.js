fcd5fc7e5e4efd1f7e4de62f8b213e9b4e597fb1
collins <kevin1520smart@gmail.com>
2023-05-05 22:48:54 +0100

    created news.js

---
+++ src/data/news.js
@@ -0,0 +1,16 @@
+const news = [
+    {
+      img: ccc,
+      date: "08",
+      info: "inland frieght a worthy solution for your business",
+      txt: "we are dedicated in creating added value for our customers by implementing modern technology in our work",
+      list: [
+        "Urgent transport solutions",
+        "Reliable & experienced staffs",
+        "Real time tracking",
+        "Custom duty",
+      ],
+    },
+  ];
+
+export default news;
