06f4b0621a74f5ac87991e01a28a2d1dba597643
collins <kevin1520smart@gmail.com>
2023-05-06 23:18:59 +0100

    styling fonts in BlogInfo

--- src/pages/BlogInfo.jsx
+++ src/pages/BlogInfo.jsx
@@ -10,10 +10,10 @@ const BlogInfo = ()=>{
         <div>
             <div>{news.slice(1, 2).map((item, idx) => <Banner key={idx} {...item} />)}</div>
             <div className='flex bg-red-500'>
-                <div>
+                <div className=' first-letter:capitalize'>
                     <p>your logistics solutions</p>
                     <p>Duis semper lacus scelerisque, aliquam leo quis, porttitor leo. Etiam lobortis dapibus libero vel porttitor. Nulla tempor elit nec feugiat tempus.Phasellus at quam hendrerit semper feugiat id nunc. Morbi quis justo velit. Duis semper lacus scelerisque, aliquam leo quis, porttitor leo Fusce lectus ex pretium[...]</p>
-                    <p>“Digitization within transport and logistics means seamless service to our customers, visibility in the supply chain, and driving a more efficient business.”</p>
+                    <p  className=' italic'>“Digitization within transport and logistics means seamless service to our customers, visibility in the supply chain, and driving a more efficient business.”</p>
                     <p>safest logistics solutions provider with integrity</p>
                     <p>Duis semper lacus scelerisque, aliquam leo quis, porttitor leo. Etiam lobortis dapibus libero vel porttitor. Nulla tempor elit nec attempus.Phasellus at quam hendrerit semper David Warner nunc. Morbi quis justo velit. Duis semper lacus scelerisque, aliquam leo quis, porttitor leo Fusce lectus ex pretium[...]</p>
                     <div>
@@ -29,39 +29,39 @@ const BlogInfo = ()=>{
                             <TwitterLogo size={20} color="#ffffff" weight="fill" />
                             <LinkedinLogo size={20} color="#ffffff" weight="fill" />
                         </div>   
-                        <p>category : shipping</p>
+                        <p>Category : Shipping</p>
                     </div>
                 </div>
                 <div>
-                    <div>
+                    <div className='capitalize'>
                         <p>categories</p>
-                        <div>
+                        <div className='flex items-center justify-between border-b border-[#d6d6d6] py-2'>
                             <p>shipping</p>
                             <p>(3)</p>
                         </div>
-                        <div>
+                        <div className='flex items-center justify-between border-b border-[#d6d6d6] py-2'>
                             <p>service</p>
                             <p>(5)</p>
                         </div>
-                        <div>
+                        <div className='flex items-center justify-between border-b border-[#d6d6d6] py-2'>
                             <p>transport</p>
                             <p>(2)</p>
                         </div>
-                        <div>
+                        <div className='flex items-center justify-between border-b border-[#d6d6d6] py-2'>
                             <p>warehouse</p>
                             <p>(5)</p>
                         </div>
-                        <div>
+                        <div className='flex items-center justify-between border-b border-[#d6d6d6] py-2'>
                             <p>transport industries</p>
                             <p>(5)</p>
                         </div>
                     </div>
-                    <div>
+                    <div className=' first-letter:capitalize'>
                         <p>how can we help</p>
                         <p>Duis semper lacus scelerisque, aliquam leo quis, porttitor leo. Etiam lobortis dapib libero vel porttitor. Nulla tempor elit nec feugiat tempus Phasellus atquam.</p>
                         <button>contact</button>
                     </div>
-                    <div>
+                    <div className=' capitalize'>
                         <div>
                             <Caption txt={'get in touch'}/>
                             <p>need help</p>
@@ -69,7 +69,7 @@ const BlogInfo = ()=>{
                         </div>
                         <div>
                             <p>email</p>
-                            <p>contact@logistics.com</p>
+                            <p className=' normal-case'>contact@logistics.com</p>
                         </div>
                     </div>
                 </div>
