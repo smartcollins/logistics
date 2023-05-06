e57c5163bc30835e409f8a91bfe78f81704f57a9
collins <kevin1520smart@gmail.com>
2023-05-06 23:05:16 +0100

    still modifying BlogInfo

--- src/pages/BlogInfo.jsx
+++ src/pages/BlogInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import news from '../data/news';
 import Banner from '../component/Banner';
 import { InstagramLogo, FacebookLogo, TwitterLogo, LinkedinLogo} from 'phosphor-react';
+import Caption from '../component/Caption';
 
 
 const BlogInfo = ()=>{
@@ -31,7 +32,47 @@ const BlogInfo = ()=>{
                         <p>category : shipping</p>
                     </div>
                 </div>
-                <div></div>
+                <div>
+                    <div>
+                        <p>categories</p>
+                        <div>
+                            <p>shipping</p>
+                            <p>(3)</p>
+                        </div>
+                        <div>
+                            <p>service</p>
+                            <p>(5)</p>
+                        </div>
+                        <div>
+                            <p>transport</p>
+                            <p>(2)</p>
+                        </div>
+                        <div>
+                            <p>warehouse</p>
+                            <p>(5)</p>
+                        </div>
+                        <div>
+                            <p>transport industries</p>
+                            <p>(5)</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p>how can we help</p>
+                        <p>Duis semper lacus scelerisque, aliquam leo quis, porttitor leo. Etiam lobortis dapib libero vel porttitor. Nulla tempor elit nec feugiat tempus Phasellus atquam.</p>
+                        <button>contact</button>
+                    </div>
+                    <div>
+                        <div>
+                            <Caption txt={'get in touch'}/>
+                            <p>need help</p>
+                            <p>(00) 112 365 489</p>
+                        </div>
+                        <div>
+                            <p>email</p>
+                            <p>contact@logistics.com</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
