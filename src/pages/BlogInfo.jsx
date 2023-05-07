4d6752b8ffe753a9d32b22acf1c22acafab3e5f2
collins <kevin1520smart@gmail.com>
2023-05-07 23:46:07 +0100

    work in progres

--- src/pages/BlogInfo.jsx
+++ src/pages/BlogInfo.jsx
@@ -3,36 +3,40 @@ import news from '../data/news';
 import Banner from '../component/Banner';
 import { InstagramLogo, FacebookLogo, TwitterLogo, LinkedinLogo} from 'phosphor-react';
 import Caption from '../component/Caption';
+import color from '../data/color';
+import cargo from '../images/blog-info.png';
 
 
 const BlogInfo = ()=>{
     return(
         <div>
             <div>{news.slice(1, 2).map((item, idx) => <Banner key={idx} {...item} />)}</div>
-            <div className='flex bg-red-500'>
-                <div className=' first-letter:capitalize'>
-                    <p>your logistics solutions</p>
-                    <p>Duis semper lacus scelerisque, aliquam leo quis, porttitor leo. Etiam lobortis dapibus libero vel porttitor. Nulla tempor elit nec feugiat tempus.Phasellus at quam hendrerit semper feugiat id nunc. Morbi quis justo velit. Duis semper lacus scelerisque, aliquam leo quis, porttitor leo Fusce lectus ex pretium[...]</p>
-                    <p  className=' italic'>“Digitization within transport and logistics means seamless service to our customers, visibility in the supply chain, and driving a more efficient business.”</p>
-                    <p>safest logistics solutions provider with integrity</p>
-                    <p>Duis semper lacus scelerisque, aliquam leo quis, porttitor leo. Etiam lobortis dapibus libero vel porttitor. Nulla tempor elit nec attempus.Phasellus at quam hendrerit semper David Warner nunc. Morbi quis justo velit. Duis semper lacus scelerisque, aliquam leo quis, porttitor leo Fusce lectus ex pretium[...]</p>
-                    <div>
-                        <img src="" alt="" />
+            <div className=' my-8 flex gap-8 w-[80%] mx-auto'>
+                <div className=' first-letter:capitalize space-y-2 bg-red-500'>
+                    <p style={{color: color.darkTxt}} className=' capitalize text-xl font-bold'>your logistics solutions</p>
+                    <p className=' text-[#666c89]'>Duis semper lacus scelerisque, aliquam leo quis, porttitor leo. Etiam lobortis dapibus libero vel porttitor. Nulla tempor elit nec feugiat tempus.Phasellus at quam hendrerit semper feugiat id nunc. Morbi quis justo velit. Duis semper lacus scelerisque, aliquam leo quis, porttitor leo Fusce lectus ex pretium[...]</p>
+                    <p style={{background: color.blueBg}} className=' italic text-white p-4 text-center'>“Digitization within transport and logistics means seamless service to our customers, visibility in the supply chain, and driving a more efficient business.”</p>
+                    <p style={{color: color.darkTxt}} className=' capitalize text-2xl font-bold'>safest logistics solutions provider with integrity</p>
+                    <p className=' text-[#666c89]'>Duis semper lacus scelerisque, aliquam leo quis, porttitor leo. Etiam lobortis dapibus libero vel porttitor. Nulla tempor elit nec attempus.Phasellus at quam hendrerit semper David Warner nunc. Morbi quis justo velit. Duis semper lacus scelerisque, aliquam leo quis, porttitor leo Fusce lectus ex pretium[...]</p>
+                    <div className='text-center text-[#666c89] capitalize'>
+                        <img className='w-full object-cover' src={cargo} alt="cargo-ship" />
                         <p>the cargo ship</p>
                     </div>
-                    <p>Duis semper lacus scelerisque, aliquam leo quis, porttitor leo. Etiam lobortis dapibus libero vel porttitor. Nulla tempor elit nec attempus.Phasellus at quam hendrerit semper David Warner nunc. Morbi quis justo velit. Duis semper lacus scelerisque, aliquam leo quis, porttitor leo Fusce lectus ex pretium[...]</p>
-                    <p>Duis semper lacus scelerisque, aliquam leo quis, porttitor leo. Etiam lobortis dapib libero vel porttitor. Nulla tempor elit nec feugiat tempus Phasellus atquam.</p>
-                    <div>
-                        <div className='flex items-center justify-between w-2/5'>
-                            <InstagramLogo size={20} color="#ffffff" />
-                            <FacebookLogo size={20} color="#ffffff" weight="fill" />
-                            <TwitterLogo size={20} color="#ffffff" weight="fill" />
-                            <LinkedinLogo size={20} color="#ffffff" weight="fill" />
+                    <div className=' text-[#666c89] space-y-4'>
+                        <p>Duis semper lacus scelerisque, aliquam leo quis, porttitor leo. Etiam lobortis dapibus libero vel porttitor. Nulla tempor elit nec attempus.Phasellus at quam hendrerit semper David Warner nunc. Morbi quis justo velit. Duis semper lacus scelerisque, aliquam leo quis, porttitor leo Fusce lectus ex pretium[...]</p>
+                        <p>Duis semper lacus scelerisque, aliquam leo quis, porttitor leo. Etiam lobortis dapib libero vel porttitor. Nulla tempor elit nec feugiat tempus Phasellus atquam.</p>
+                    </div>
+                    <div className='flex items-center justify-between'>
+                        <div className='flex items-center justify-between w-1/5'>
+                            <InstagramLogo size={20} color="#1c1f35" />
+                            <FacebookLogo size={20} color="#1c1f35" weight="fill" />
+                            <TwitterLogo size={20} color="#1c1f35" weight="fill" />
+                            <LinkedinLogo size={20} color="#1c1f35" weight="fill" />
                         </div>   
-                        <p>Category : Shipping</p>
+                        <p className='text-[#666c89]'>Category : Shipping</p>
                     </div>
                 </div>
-                <div>
+                <div className='bg-red-300'>
                     <div className='capitalize'>
                         <p>categories</p>
                         <div className='flex items-center justify-between border-b border-[#d6d6d6] py-2'>
