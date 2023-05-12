51a58c1c19f9a10da668f0dc07fd2e59b63c1692
collins <kevin1520smart@gmail.com>
2023-05-13 00:07:03 +0100

    modified Protection

--- src/pages/Protection.jsx
+++ src/pages/Protection.jsx
@@ -9,17 +9,17 @@ const Protection = () => {
     return (
         <div>
             <div>{banner.slice(14, 15).map((item, idx) => <Banner key={idx} {...item} />)}</div>
-            <div style={{ backgroundColor: color.blueBg }} className='h-[45vh] my-8 mx-auto w-2/4 capitalize text-center flex flex-col items-center'>
-                <div className='m-auto w-2/4 h-2/4 flex flex-col items-center justify-between'>
+            <div style={{ backgroundColor: color.blueBg }} className='h-[45vh] my-8 lg:mx-auto lg:w-3/4 capitalize text-center flex flex-col items-center'>
+                <div className='space-y-4 w-full px-4 my-auto md:px-0 md:m-auto md:w-3/4 lg:m-auto lg:w-2/4 lg:h-2/4 flex flex-col items-center justify-between'>
                     <p className='text-white text-2xl font-bold'>enter password</p>
-                    <div className=' w-full flex items-center gap-4'>
-                        <input className=' bg-transparent text-white indent-4 placeholder:capitalize caret-[#999999] py-2 border border-[#4e5683] outline-[#4e5683]' type="text" placeholder='enter your password' />
+                    <div className='w-full flex items-center justify-center gap-4'>
+                        <input className=' w-full bg-transparent text-white indent-4 placeholder:capitalize caret-[#999999] py-2 border border-[#4e5683] outline-[#4e5683]' type="text" placeholder='enter your password' />
                         <button style={{
                             background: `linear-gradient(94.06deg,
                                 ${color.grdt} -1.21%, ${color.grdt2} 58.66%,
                                 ${color.grdt3} 116.84%)`
                         }}
-                            className='w-full font-medium text-base py-2'>submit</button>
+                            className='w-2/4 font-medium text-base py-2'>submit</button>
                     </div>
                 </div>
             </div>
