cf19f3a26ea38082aa1302411ac8f037c67694a6
collins <kevin1520smart@gmail.com>
2023-05-19 00:22:08 +0100

    modified Blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -13,7 +13,7 @@ const Blog = () => {
         <div>
             <div>{banner.slice(6, 7).map((item, idx) => <Banner key={idx} {...item} />)}</div>
             <Caption mid={true} dark={true} txt={'our blog'} txt2={'our latest news'} />
-            <div className=' space-y-4 mx-4 lg:my-8 lg:w-3/5 lg:mx-auto bg-red-500'>
+            <div className=' space-y-4 mx-4 lg:my-8 lg:w-3/4 lg:mx-auto xl:w-3/5'>
                 {items}
             </div>
             <Footer />
