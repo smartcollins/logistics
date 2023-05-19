f3bac27bcd42c68f3b879ce149a068b3248b9264
collins <kevin1520smart@gmail.com>
2023-05-19 22:52:21 +0100

    added Footer to Project

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import banner from '../data/banner';
 import Banner from '../component/Banner';
 import color from '../data/color';
+import Footer from '../component/Footer';
 
 
 const Project = ()=>{
@@ -19,7 +20,7 @@ const Project = ()=>{
         txt2: 'owned vehicles'
     }]
 
-    const end = endAry.map((item,idx)=><div key={idx} className='flex items-center justify-center gap-2 capitalize w-full first:border-x-none last:border-x-none border py-4'>
+    const end = endAry.map((item,idx)=><div key={idx} className='flex items-center justify-center gap-2 capitalize w-full first:border-x-none last:border-x-none border py-8'>
     <p className='font-bold text-4xl'>{item.txt}</p>
     <span style={{ background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` }} className=' w-3 h-3'></span>
     <p>{item.txt2}</p>
@@ -30,6 +31,7 @@ const Project = ()=>{
             <div style={{color: color.darkTxt}} className='flex justify-between'>
                 {end}
             </div>
+            <Footer/>
         </div>
     )
 }
