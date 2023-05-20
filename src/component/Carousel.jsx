8328367d1e966945f5d2bdea9e695fb0f018840c
collins <kevin1520smart@gmail.com>
2023-05-20 16:23:26 +0100

    modified Carousel

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const Carousel = ()=>{
-    var settings = {
+const Carousel = ({items})=>{
+    const settings = {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplaySpeed: 2500,
+        speed: 500,
         dots: true,
         infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
+        swipeToSlide: true,
+        autoplay: true
+        
       };
     return(
-        <div>
+        <div className='bg-red-200'>
             <Slider {...settings}>
-                
+                {items}
             </Slider>
         </div>
     )
