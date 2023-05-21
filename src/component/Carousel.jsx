7ec1503639d68905a3028b09aacf7308faccbe76
collins <kevin1520smart@gmail.com>
2023-05-21 21:50:44 +0100

    formmated Carousel

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -3,34 +3,34 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const Carousel = ({items,show,scroll,dot})=>{
-    const settings = {
-        slidesToShow: show,
-        slidesToScroll: scroll,
-        autoplaySpeed: 2500,
-        speed: 500,
-        dots: dot,
-        infinite: true,
-        swipeToSlide: true,
-        autoplay: true,
-        responsive: [
-            {
-              breakpoint: 640,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                autoplaySpeed: 2500,
-              },
-            },
-          ],
-        
-      };
-    return(
-      <Slider {...settings}>
-          {items}
-      </Slider>
-    )
+const Carousel = ({ items, show, scroll, dot }) => {
+  const settings = {
+    slidesToShow: show,
+    slidesToScroll: scroll,
+    autoplaySpeed: 2500,
+    speed: 500,
+    dots: dot,
+    infinite: true,
+    swipeToSlide: true,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          autoplaySpeed: 2500,
+        },
+      },
+    ],
+
+  };
+  return (
+    <Slider {...settings}>
+      {items}
+    </Slider>
+  )
 }
 
 
