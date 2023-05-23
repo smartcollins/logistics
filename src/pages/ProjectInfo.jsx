3abf99cb011b113146b4c50cee6fc1d062f94a51
collins <kevin1520smart@gmail.com>
2023-05-23 22:41:07 +0100

    work in progress

--- src/pages/ProjectInfo.jsx
+++ src/pages/ProjectInfo.jsx
@@ -33,8 +33,10 @@ const ProjectInfo = () => {
 
     const list = listAry.map((item, idx) => (
         <div key={idx} className="flex items-center gap-4">
-            <p className=" font-medium">{item.txt}</p>
-            <span>:</span>
+            <div className="flex gap-2 w-1/3 md:w-auto">
+                <p className=" font-medium">{item.txt}</p>
+                <span>:</span>
+            </div>
             <p>{item.txt2}</p>
         </div>
     ));
@@ -66,18 +68,18 @@ const ProjectInfo = () => {
     return (
         <div>
             <Banner img={banner} txt={"warehouse transportation"} info={"air cargo transportataion"} />
-            <div style={{ background: color.grayBg }} className="h-full py-4 lg:py-12">
-                <div className="lg:flex items-center gap-8 mx-auto capitalize w-11/12 space-y-4 lg:w-3/4">
-                    <div className="bg-red-300 lg:w-3/4">
+            <div style={{ background: color.grayBg }} className="h-full py-4 md:py-8 lg:py-12">
+                <div className="gap-8 mx-auto capitalize w-11/12 space-y-4 md:flex md:items-center md:space-y-0 lg:w-3/4">
+                    <div className="lg:w-3/4">
                         <img className="w-full object-cover" src={img} alt="project detail"/>
                     </div>
                     <div className="w-full">
-                        <p className=" font-bold text-xl text-center lg:text-left">project details</p>
+                        <p className=" font-bold text-xl text-center md:text-left">project details</p>
                         <div className="mt-4 space-y-4">{list}</div>
                     </div>
                 </div>
             </div>
-            <div className="w-3/4 mx-auto">
+            <div className="w-11/12 mx-auto lg:w-3/4">
                 <div className="my-8 space-y-4">
                     <p className=" text-2xl font-bold capitalize">wharehouse transportation</p>
                     <p className=" text-sm text-[#666c89]">Duis semper lacus scelerisque, aliquam leo quis, porttitor leo. Etiam lobortis dapibus libero vel porttitor. Nulla tempor elit nec feugiat tempus.Phasellus at quam id elit hendrerit semper feugiat id nunc. Morbi quis justo velit. Duis semper lacus scelerisque, aliquam leo quis, porttitor leo. Fusce lectus ex, pretium efficitur suscipit sed, faucibus vel elit Integer adipiscing erat eget risus sollicitudin pellentesque non erat. Maecenas nibh dolor malesuada sagittis accumsan ipsum. Pellentesque ultrices ultrices sapien, nec tincidunt nunc posuere.</p>
