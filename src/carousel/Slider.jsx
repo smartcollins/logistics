0ed89b8a0c5204521d629be5d41fe93fa0b83f0e
collins <kevin1520smart@gmail.com>
2023-05-23 15:16:30 +0100

    created Slider

---
+++ src/carousel/Slider.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ArrowLeft, ArrowRight } from 'phosphor-react';
+
+
+const Slider = ()=>{
+    return(
+        <div>
+            <ArrowLeft size={20} weight='fill'/>
+            <ArrowRight size={20} weight='fill'/>
+        </div>
+    )
+}
+
+
+export default  Slider;
