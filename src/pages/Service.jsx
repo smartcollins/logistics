adf8ea37a6b8c645c6c87fa561d3328ae469cc2f
collins <kevin1520smart@gmail.com>
2023-05-25 21:44:06 +0100

    added Footer to Service

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import banner from '../data/banner';
+import Banner from '../component/Banner';
+import Footer from '../component/Footer';
 
 
 const Service = ()=>{
     return(
         <div>
             <div>{banner.slice(3, 4).map((item, idx) => <Banner key={idx} {...item} />)}</div>
+            <Footer/>
         </div>
     )
 }
