a9223d4d7097c7b2dd485a32d17d5d611764b8e1
collins <kevin1520smart@gmail.com>
2023-05-26 22:04:30 +0100

    still modifying Service

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -8,16 +8,16 @@ import service from '../data/service';
 
 
 const Service = ()=>{
-    const items = service.map((item,idx) => <div key={idx} className='bg-red-400 w-2/4'>
+    const items = service.map((item,idx) => <div key={idx} className='bg-red-400 group'>
     <img src={item.icon} alt='icon-logo' />
-    <p className=' text-[#1c1f35] hover:text-[#ffb82b] text-2xl capitalize'>sea transport service</p>
+    <p className=' text-[#1c1f35] group-hover:text-[#ffb82b] text-2xl capitalize'>{item.txt}</p>
     <p style={{color: color.grayTxt}}>following the quality of our service thus having gained trust of our many clients</p>
 </div>)
     return(
         <div>
             <div>{banner.slice(3, 4).map((item, idx) => <Banner key={idx} {...item} />)}</div>
             <Caption mid={true} dark={true} txt={'what we do'} txt2={'logistics services'}/>
-            <div>
+            <div className='grid grid-cols-3 gap-4'>
                 {/* <div className='bg-red-400 w-2/4'>
                     <img src={ship} alt="ship-logo" />
                     <p className=' text-[#1c1f35] hover:text-[#ffb82b] text-2xl capitalize'>sea transport service</p>
