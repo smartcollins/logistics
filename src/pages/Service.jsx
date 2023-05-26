a0d59ca1d493bad0ab950cf626c8c1b4da3b5ce0
collins <kevin1520smart@gmail.com>
2023-05-26 22:31:43 +0100

    added more icons and image

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -5,26 +5,29 @@ import Banner from '../component/Banner';
 import Caption from '../component/Caption'
 import Footer from '../component/Footer';
 import service from '../data/service';
+import Review from '../component/Review';
 
 
 const Service = ()=>{
-    const items = service.map((item,idx) => <div key={idx} className='bg-red-400 group'>
+    const items = service.map((item,idx) => <div key={idx} className='group bg-red-500'>
     <img src={item.icon} alt='icon-logo' />
     <p className=' text-[#1c1f35] group-hover:text-[#ffb82b] text-2xl capitalize'>{item.txt}</p>
-    <p style={{color: color.grayTxt}}>following the quality of our service thus having gained trust of our many clients</p>
+    <p style={{color: color.grayTxt}} className=' w-2/3'>following the quality of our service thus having gained trust of our many clients</p>
 </div>)
     return(
         <div>
             <div>{banner.slice(3, 4).map((item, idx) => <Banner key={idx} {...item} />)}</div>
-            <Caption mid={true} dark={true} txt={'what we do'} txt2={'logistics services'}/>
-            <div className='grid grid-cols-3 gap-4'>
-                {/* <div className='bg-red-400 w-2/4'>
-                    <img src={ship} alt="ship-logo" />
-                    <p className=' text-[#1c1f35] hover:text-[#ffb82b] text-2xl capitalize'>sea transport service</p>
-                    <p style={{color: color.grayTxt}}>following the quality of our service thus having gained trust of our many clients</p>
-                </div> */}
-                {items}
+            <div className=' text-center my-8'>
+                <Caption mid={true} dark={true} txt={'what we do'} txt2={'logistics services'}/>
+                <div className='grid grid-cols-3 gap-4 text-left'>
+                    {items}
+                </div>
+                <button style={{background: color.blueBg}} className='text-white py-2 px-8 my-4'>More Works</button>
             </div>
+            <div>
+                <Caption mid={true} dark={true} txt={'our goodness'} txt2={'how we work'}/>
+            </div>
+            <Review/>
             <Footer/>
         </div>
     )
