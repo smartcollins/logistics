52368aaef1cd0e17e55ae05a3078e8fa3f960a55
collins <kevin1520smart@gmail.com>
2023-05-26 21:51:00 +0100

    created service.js

---
+++ src/data/service.js
@@ -0,0 +1,28 @@
+import ship from '../icons/ship-icon.svg'
+import build from '../icons/build-icon.svg'
+import flight from '../icons/flight-icon.svg'
+import screen from '../icons/screen-icon.svg'
+import truck from '../icons/truck-icon.svg'
+import user from '../icons/cust-icon.svg'
+
+const service = [{
+    icon: ship,
+    txt: 'sea transport services'
+},{
+    icon: build,
+    txt: 'warehousing services'
+},{
+    icon: flight,
+    txt: 'air fright services'
+},{
+    icon: screen,
+    txt: 'project & exhibition'
+},{
+    icon: truck,
+    txt: 'local shipping services'
+},{
+    icon: user,
+    txt: 'customer clearance'
+}]
+
+export default service;
