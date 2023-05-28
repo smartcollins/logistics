63b613180cfeadfe3b0376595a08342a4d0c907a
collins <kevin1520smart@gmail.com>
2023-05-28 21:56:27 +0100

    created stat.js

---
+++ src/data/stat.js
@@ -0,0 +1,15 @@
+const stat = [{
+    txt: '24',
+    txt2: 'our location'
+}, {
+    txt: '1294',
+    txt2: 'delivered package'
+}, {
+    txt: '3594',
+    txt2: 'satisfied clients'
+}, {
+    txt: '247+',
+    txt2: 'owned vehicles'
+}]
+
+export default stat;
