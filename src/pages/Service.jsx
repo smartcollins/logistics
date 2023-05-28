2406f4ccd2e75f9f842a321eaa84841c73c2c959
collins <kevin1520smart@gmail.com>
2023-05-28 21:46:30 +0100

    minor correction in Service

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -16,7 +16,7 @@ const Service = ()=>{
     const items = service.map((item,idx) => <div key={idx} className='group'>
     <img src={item.icon} alt='icon-logo' />
     <p className=' text-[#1c1f35] group-hover:text-[#ffb82b] text-2xl capitalize'>{item.txt}</p>
-    <p style={{color: color.grayTxt}} className=' w-2/3'>following the quality of our service thus having gained trust of our many clients</p>
+    <p style={{color: color.grayTxt}} className='w-3/4'>following the quality of our service thus having gained trust of our many clients</p>
 </div>)
 
     const itmAry = [{
@@ -41,12 +41,12 @@ const Service = ()=>{
     return(
         <div>
             <div>{banner.slice(3, 4).map((item, idx) => <Banner key={idx} {...item} />)}</div>
-                <div className=' text-center my-8'>
+                <div className=' text-center my-8 w-3/4 mx-auto space-y-8'>
                     <Caption mid={true} dark={true} txt={'what we do'} txt2={'logistics services'}/>
-                    <div className='grid grid-cols-3 gap-4 text-left'>
+                    <div className='grid grid-cols-3 gap-8 text-left'>
                         {items}
                     </div>
-                    <button style={{background: color.blueBg}} className='text-white py-2 px-8 my-4'>More Works</button>
+                    <button style={{background: color.blueBg}} className='text-white py-2 px-8'>More Works</button>
                 </div>
                 <div style={{background: color.grayBg}}>
                     <div className='w-5/6 mx-auto py-8'>
