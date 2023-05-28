36154945f54cdcd8b0a0fb139a79c9c4383b4d3f
collins <kevin1520smart@gmail.com>
2023-05-28 21:24:05 +0100

    removed background colors

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -13,7 +13,7 @@ import Review from '../component/Review';
 
 
 const Service = ()=>{
-    const items = service.map((item,idx) => <div key={idx} className='group bg-red-500'>
+    const items = service.map((item,idx) => <div key={idx} className='group'>
     <img src={item.icon} alt='icon-logo' />
     <p className=' text-[#1c1f35] group-hover:text-[#ffb82b] text-2xl capitalize'>{item.txt}</p>
     <p style={{color: color.grayTxt}} className=' w-2/3'>following the quality of our service thus having gained trust of our many clients</p>
@@ -30,7 +30,7 @@ const Service = ()=>{
         txt: 'care for environment'
     }]
 
-    const itm = itmAry.map((item,idx)=><div key={idx} className='flex items-start gap-4 bg-red-400'>
+    const itm = itmAry.map((item,idx)=><div key={idx} className='flex items-start gap-4'>
         <img src={item.icon} alt="icons" />
         <div className='mt-4'>
             <p className='text-xl capitalize'>{item.txt}</p>
@@ -49,11 +49,11 @@ const Service = ()=>{
                     <button style={{background: color.blueBg}} className='text-white py-2 px-8 my-4'>More Works</button>
                 </div>
                 <div style={{background: color.grayBg}}>
-                    <div className='w-5/6 mx-auto py-8 bg-red-500'>
+                    <div className='w-5/6 mx-auto py-8'>
                     <Caption mid={true} dark={true} txt={'our goodness'} txt2={'how we work'}/>
                     <div className='flex gap-8 h-[50vh]'>
                         <img className='w-2/4 object-cover' src={side} alt="service side" />
-                        <div className=' space-y-4 bg-red-200 w-2/4'>
+                        <div className=' space-y-4 w-2/4'>
                                 {itm}
                         </div>
                     </div>
