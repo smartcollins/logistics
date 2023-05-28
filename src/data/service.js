1099f43bc909ae76824d187b43fa2c549edd3f9a
collins <kevin1520smart@gmail.com>
2023-05-28 21:45:52 +0100

    made comment in service.js

--- src/data/service.js
+++ src/data/service.js
@@ -4,6 +4,8 @@ import flight from '../icons/flight-icon.svg'
 import screen from '../icons/screen-icon.svg'
 import truck from '../icons/truck-icon.svg'
 import user from '../icons/cust-icon.svg'
+// flight svg property --- width="74" height="46" viewBox="0 0 74 58"
+// reference --- svg width="56" height="58" viewBox="0 0 56 58"
 
 const service = [{
     icon: ship,
