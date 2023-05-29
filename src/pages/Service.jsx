53bf0fdf8ddf917baf5d6f1ef65f84796b66d45b
collins <kevin1520smart@gmail.com>
2023-05-29 12:41:25 +0100

    still modifying Service

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -32,9 +32,9 @@ const Service = () => {
         txt: 'care for environment'
     }]
 
-    const itm = itmAry.map((item, idx) => <div key={idx} className='flex gap-4 xl:items-start'>
+    const itm = itmAry.map((item, idx) => <div key={idx} className='flex gap-4 lg:items-start'>
         <img src={item.icon} alt="icons" />
-        <div className='xl:mt-4'>
+        <div className='lg:mt-4'>
             <p className='text-xl capitalize'>{item.txt}</p>
             <p>Leverage agile frameworks to provide arobust synopis for strategy foster collaborative thinking</p>
         </div>
@@ -56,18 +56,18 @@ const Service = () => {
             <div style={{ background: color.grayBg }}>
                 <div className=' w-11/12 mx-auto py-4 xl:py-8 xl:w-5/6'>
                     <Caption mid={true} dark={true} txt={'our goodness'} txt2={'how we work'} />
-                    <div className='flex flex-wrap gap-8 xl:h-[50vh]'>
-                        <img className='object-cover md:w-2/4' src={side} alt="service side" />
+                    <div className='flex flex-wrap gap-8 lg:flex-nowrap'>
+                        <img className='object-cover md:h-[50vh] md:w-full lg:h-auto lg:w-2/4' src={side} alt="service side" />
                         <div className=' space-y-4 xl:w-2/4'>
                             {itm}
                         </div>
                     </div>
                 </div>
             </div>
-            <div className='mx-auto my-8 xl:w-3/4'>
+            <div className='mx-auto md:w-11/12 my-8 lg:w-3/4'>
                 <Review />
             </div>
-            <div className='w-4/5 mx-auto'>
+            <div className='xl:w-4/5 mx-auto'>
                 <Stat />
             </div>
             <Footer />
