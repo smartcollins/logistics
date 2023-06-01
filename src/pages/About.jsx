82dca7cca20366ec9608641e0b8bc3fc23a4f846
collins <kevin1520smart@gmail.com>
2023-06-01 15:04:40 +0100

    modified About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import banner from '../data/banner';
+import color from '../data/color';
 import Banner from '../component/Banner';
+import Work from '../component/Work';
 
 
 const About = ()=>{
     return(
         <div>
             <div>{banner.slice(1, 2).map((item, idx) => <Banner key={idx} {...item} />)}</div>
+            <div style={{background: color.grayBg}} className=''>
+                <div className='text-center py-8 mx-auto w-3/4'>
+                    <Work/>
+                </div>
+            </div>
         </div>
     )
 }
