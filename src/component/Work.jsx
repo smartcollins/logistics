9a6e45b3093eedd638f62034435fe58157214dad
collins <kevin1520smart@gmail.com>
2023-06-01 14:43:29 +0100

    modified Work

--- src/component/Work.jsx
+++ src/component/Work.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import color from '../data/color';
 import work from '../data/work';
 
 
 const Work = ()=>{
     return(
-        <div>
+        <div className=' space-y-4 gap-8 md:space-y-0 md:grid md:grid-cols-2 lg:grid-cols-3 lg:w-11/12 mx-auto'>
             {
                 work.map((item,idx)=><div key={idx} className='group md:text-left'>
                     <img className='mx-auto md:m-0' src={item.icon} alt='icon-logo' />
