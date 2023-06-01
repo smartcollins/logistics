0b2ec9a9e61fd9b887157a1a74297c6885c0abe5
collins <kevin1520smart@gmail.com>
2023-06-01 14:39:14 +0100

    modified Work

--- src/component/Work.jsx
+++ src/component/Work.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import work from '../data/work';
 
 
 const Work = ()=>{
     return(
-        <div></div>
+        <div>
+            {
+                work.map((item,idx)=><div key={idx} className='group md:text-left'>
+                    <img className='mx-auto md:m-0' src={item.icon} alt='icon-logo' />
+                    <p className=' text-[#1c1f35] group-hover:text-[#ffb82b] text-2xl capitalize'>{item.txt}</p>
+                    <p style={{ color: color.grayTxt }} className='xl:w-3/4'>following the quality of our service thus having gained trust of our many clients</p>
+                </div>)
+            }
+        </div>
     )
 }
 
