5fc120b4aac06d0dcb9c2965d1c057bab6e4d9b9
collins <kevin1520smart@gmail.com>
2023-06-02 20:02:58 +0100

    created card.js

---
+++ src/data/card.js
@@ -0,0 +1,34 @@
+const card = [
+  {
+    img: team,
+    name: "jessica arow",
+    title: "warehouse head",
+  },
+  {
+    img: team2,
+    name: "kathleen smith",
+    title: "transport manager",
+  },
+  {
+    img: team3,
+    name: "rebecca tylor",
+    title: "cargo head",
+  },
+  {
+    img: team4,
+    name: "ria jackson",
+    title: "operational head",
+  },
+  {
+    img: team5,
+    name: "kane williams",
+    title: "country head",
+  },
+  {
+    img: team6,
+    name: "lisara tylor",
+    title: "finance head",
+  },
+];
+
+export default card;
