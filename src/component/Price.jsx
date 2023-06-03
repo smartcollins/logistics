48a5fcdd9e0bc147750f4cba2295d5f97e486674
collins <kevin1520smart@gmail.com>
2023-06-03 08:04:34 +0100

    minor modifications

--- src/component/Price.jsx
+++ src/component/Price.jsx
@@ -6,9 +6,11 @@ import Caption from './Caption';
 
 const Price = ()=>{
     return(
-        <div style={{ color: color.darkTxt }} className=' capitalize mx-auto my-8 md:py-8 md:w-3/4'>
-            <Caption mid={true} dark={true} txt={'pricing'} txt2={'our best pricing'} />
-            <div className='flex flex-wrap gap-4 text-center py-4 lg:flex-nowrap'>
+        <div style={{ color: color.darkTxt }} className=' capitalize mx-auto md:w-3/4'>
+            <div className=' mt-8 md:my-0'>
+                <Caption mid={true} dark={true} txt={'pricing'} txt2={'our best pricing'} />
+            </div>
+            <div className='flex flex-wrap text-center py-4 md:gap-4 lg:flex-nowrap'>
             {
                 price.map((item,idx)=><div key={idx} style={item.dark ? { backgroundColor: color.blueBg, color: 'white' } : { backgroundColor: color.grayBg }} className='bg-red-300 w-full flex flex-col space-y-4 p-8'>
                 <p className=' font-medium text-xl'>{item.title}</p>
@@ -21,7 +23,7 @@ const Price = ()=>{
                         item.txt.map((item, idx) => <p key={idx} className=' last:border-b border-t border-[#D6D6D6] py-2'>{item}</p>)
                     }
                 </div>
-                <button style={item.dark ? { color: color.darkTxt, background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` } : { backgroundColor: color.blueBg, color: 'white' }} className='bg-red-200 w-2/4 py-2 mx-auto font-medium'>choose plan</button>
+                <button style={item.dark ? { color: color.darkTxt, background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` } : { backgroundColor: color.blueBg, color: 'white' }} className='bg-red-200 w-2/4 py-2 mx-auto font-medium rounded-sm md:w-1/3 lg:w-3/4 xl:w-2/4'>choose plan</button>
             </div>)
             }
             </div>
