11451a0b05be298b17b591ee806ac783cf39fa3f
collins <kevin1520smart@gmail.com>
2023-06-03 07:16:26 +0100

    created price.js

---
+++ src/data/price.js
@@ -0,0 +1,16 @@
+const price = [{
+    title: 'standard',
+    cost: 39,
+    txt: ['single truck', 'full insurance', '500 km', 'Real-time-shopping'],
+}, {
+    title: 'standard',
+    cost: 59,
+    dark: true,
+    txt: ['single truck', 'full insurance', '500 km', 'Real-time-shopping'],
+}, {
+    title: 'standard',
+    cost: 89,
+    txt: ['double truck', 'full insurance', 'unlimited', 'Real-time-shopping'],
+}]
+
+export default price;
