cacf82c424f5f148742954c9bf99f6680c7b5923
collins <kevin1520smart@gmail.com>
2023-06-05 21:18:07 +0100

    modified About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,25 +11,26 @@ import Card from '../component/Card';
 import Review from '../component/Review'
 import Price from '../component/Price';
 import Asked from '../component/Asked';
+import Footer from '../component/Footer';
 
 
 
-const About = ()=>{
-    const btnAry = ['our goal','our approach','our target']
-    const btn = btnAry.map((item,idx)=><button key={idx} className=' first:bg-[#FFB82B] first:text-white bg-[#f4f4f4] text-[#1c1f35] capitalize py-2 rounded w-2/4 font-medium'>{item}</button>)
-    return(
-        <div>
+const About = () => {
+    const btnAry = ['our goal', 'our approach', 'our target']
+    const btn = btnAry.map((item, idx) => <button key={idx} className=' first:bg-[#FFB82B] first:text-white bg-[#f4f4f4] text-[#1c1f35] capitalize py-2 rounded w-2/4 font-medium'>{item}</button>)
+    return (
+        <div className=' space-y-16'>
             <div>{banner.slice(1, 2).map((item, idx) => <Banner key={idx} {...item} />)}</div>
             <div>
                 <div className="flex flex-wrap items-center justify-between w-11/12 mx-auto xl:flex-nowrap xl:w-3/4 xl:gap-8">
                     <div className='md:h-[50vh] xl:h-full xl:w-2/4'>
-                        <img className="w-full h-full object-cover " src={side} alt="side-ad"/>
+                        <img className="w-full h-full object-cover" src={side} alt="side-ad" />
                         {/* <img className="w-full object-cover md:h-[50vh] xl:h-full xl:w-2/5 " src={side} alt="side-ad"/> */}
                     </div>
                     <div className="text-left space-y-4 xl:w-2/4">
                         <div className="my-4 capitalize">
                             <Caption txt="about us" txt2="our company overview" />
-                            <p className=" normal-case text-[#666c89]"> Leverage agile frameworks to provide a robust synopsis for strategy foster collaborative thinking to further the overall value proposition.
+                            <p className="normal-case text-[#666c89]">Leverage agile frameworks to provide a robust synopsis for strategy foster collaborative thinking to further the overall value proposition.
                             </p>
                         </div>
                         <div className='flex items-center gap-4'>
@@ -40,27 +41,28 @@ const About = ()=>{
                     </div>
                 </div>
             </div>
-            <div style={{background: color.grayBg}} className=''>
+            <div style={{ background: color.grayBg }}>
                 <div className='text-center py-8 mx-auto w-3/4'>
-                    <Work/>
+                    <Work />
                 </div>
             </div>
-            <div className="space-y-4 w-11/12 gap-8 my-8 mx-auto md:grid md:space-y-0 md:grid-cols-2 xl:grid-cols-3 xl:w-3/4">
-                {card.slice(0,3).map((item, idx) => <div key={idx} className='md:last:hidden xl:last:block'><Card {...item} /></div>)}
+            <div className="space-y-4 w-11/12 gap-8 mx-auto md:grid md:space-y-0 md:grid-cols-2 xl:grid-cols-3 xl:w-3/4">
+                {card.slice(0, 3).map((item, idx) => <div key={idx} className='md:last:hidden xl:last:block'><Card {...item} /></div>)}
             </div>
-            <div style={{background: color.grayBg}} className=''>
+            <div style={{ background: color.grayBg }}>
                 <div className='text-center py-8 mx-auto w-3/4'>
-                    <Review/>
+                    <Review />
                 </div>
             </div>
-            <Price/>
-            <Asked/>
-            <div className="my-8 xl:my-8">
-                <img className=" mx-auto h-16 w-11/12 md:object-cover md:h-full xl:w-3/4" src={sponsor} alt="delivery"/>
+            <Price />
+            <Asked />
+            <div>
+                <img className=" mx-auto h-16 w-11/12 md:object-cover md:h-full xl:w-3/4" src={sponsor} alt="delivery" />
             </div>
             {/* <div className='my-8 h-full mx-auto xl:h-[15vh] xl:w-3/4'>
                 <img className='mx-auto xl:w-full h-full object-cover' src={sponsor} alt="client-logo" />
             </div> */}
+            <Footer />
         </div>
     )
 }
