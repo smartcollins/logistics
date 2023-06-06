dc78299cebd06655f6fd2fb7d6731ca43488b038
collins <kevin1520smart@gmail.com>
2023-06-06 20:57:46 +0100

    added Footer to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import banner from '../data/banner';
 import Banner from '../component/Banner';
+import Footer from '../component/Footer';
 
 
 const Home = ()=>{
     return(
         <div>
             <div>{banner.slice(0, 1).map((item, idx) => <Banner key={idx} {...item} />)}</div>
+            <Footer/>
         </div>
     )
 }
