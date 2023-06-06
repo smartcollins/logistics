0090c7918a4b9cd0c9f23c3d17ec7cd7c48a5d83
collins <kevin1520smart@gmail.com>
2023-06-06 21:31:41 +0100

    created input.js

---
+++ src/data/input.js
@@ -0,0 +1,14 @@
+const input = [{
+    type: 'text',
+    place: 'your name*'
+}, {
+    type: 'email',
+    place: 'email*'
+}, {
+    type: 'number',
+    place: 'phone number*'
+}, {
+    type: 'text',
+    place: 'city*'
+}];
+export default input;
