7a37fe449fee59c6313487e598b1007e3e3e344e
collins <kevin1520smart@gmail.com>
2023-06-06 21:05:52 +0100

    added News to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import banner from '../data/banner';
 import Banner from '../component/Banner';
 import Footer from '../component/Footer';
+import news from '../data/news';
+import News from '../component/News';
 
 
 const Home = ()=>{
     return(
         <div>
-            <div>{banner.slice(0, 1).map((item, idx) => <Banner key={idx} {...item} />)}</div>
+            <div>{banner.slice(0, 1).map((item, idx) => <Banner key={idx} {...item} />)}</div>\
+            <div className=' space-y-4 mx-4 lg:my-8 lg:w-3/4 lg:mx-auto xl:w-3/5'>
+                {news.map((item,idx)=><News key={idx} {...item}/>)}
+            </div>
             <Footer/>
         </div>
     )
