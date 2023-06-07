f8d9d4142d09178f652f987efedb392e78ebeb81
collins <kevin1520smart@gmail.com>
2023-06-07 21:32:02 +0100

    created Input

---
+++ src/component/Input.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import input from '../data/input';
+
+
+const Input = ()=>{
+    input.map((item, idx) => <input key={idx} type={item.type} placeholder={item.place} className=' indent-4 text-2xl placeholder:text-white lg:placeholder:text-2xl placeholder:text-lg placeholder:text-white/80 placeholder:capitalize bg-transparent border border-white h-16 w-full' />)
+}
+
+
+export default Input; 
