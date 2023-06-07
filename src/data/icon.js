e43044fe17d30c3798f5447a164d59759c753a6b
collins <kevin1520smart@gmail.com>
2023-06-07 21:42:40 +0100

    created icpn

---
+++ src/data/icon.js
@@ -0,0 +1,15 @@
+const icon = [
+  {
+    icon: EnvelopeSimple,
+    txt: "contact@logistics.com",
+  },
+  {
+    icon: Phone,
+    txt: "(00) 112 365 489",
+  },
+  {
+    icon: Clock,
+    txt: "mon - sat 9.00 - 18.00 sunday closed",
+  },
+];
+export default icon;
