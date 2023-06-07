7760e68b7465a34c8212a4b1e571de274e351876
collins <kevin1520smart@gmail.com>
2023-06-07 21:46:36 +0100

    formatted Icon

--- src/component/Icon.jsx
+++ src/component/Icon.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import icon from '../data/icon';
+import React from "react";
+import icon from "../data/icon";
 
-const Icon = ()=>{
-    return(
-        icon.map((item, idx) => <div key={idx} className='md:w-2/4 flex gap-4 justify-between md:justify-normal md:flex-col items-center'>
-        {
-            React.createElement(item.icon, {
-                size: 30, color: '#ffffff', className: 'w-fit h-fit p-4 bg-[#273270] border border-[#273270] rounded-full'
-            })
-        }
-        <p className='w-full'>{item.txt}</p>
-    </div>)
-    )
-}
+const Icon = () => {
+  return icon.map((item, idx) => (
+    <div
+      key={idx}
+      className="md:w-2/4 flex gap-4 justify-between md:justify-normal md:flex-col items-center">
+      {React.createElement(item.icon, {
+        size: 30,
+        color: "#ffffff",
+        className: "w-fit h-fit p-4 bg-[#273270] border border-[#273270] rounded-full",
+      })}
+      <p className="w-full">{item.txt}</p>
+    </div>
+  ));
+};
 
-
-export default Icon; 
+export default Icon;
