d2cfd9642c0262ce243ad99dcaaf677a8f850dba
collins <kevin1520smart@gmail.com>
2023-06-08 22:08:23 +0100

    modifying Icon

--- src/component/Icon.jsx
+++ src/component/Icon.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import icon from "../data/icon";
 
-const Icon = () => {
+const Icon = ({alt}) => {
   return icon.map((item, idx) => (
     <div
       key={idx}
-      className="md:w-2/4 flex gap-4 justify-between md:justify-normal md:flex-col items-center">
+      className={alt ? "text-white flex items-center gap-4 capitalize w-1/5 bg-red-500" : "md:w-2/4 flex gap-4 justify-between md:justify-normal md:flex-col items-center text-white bg-red-400"}>
       {React.createElement(item.icon, {
         size: 30,
-        color: "#ffffff",
+        color: alt ? "#F6B426" : "#ffffff",
         className: "w-fit h-fit p-4 bg-[#273270] border border-[#273270] rounded-full",
       })}
-      <p className="w-full">{item.txt}</p>
+      <div>
+        {alt? <p>{item.type}</p> : null}
+        <p className="w-full">{item.txt}</p>
+      </div>
     </div>
   ));
 };
