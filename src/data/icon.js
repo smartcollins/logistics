1efe2560886e544f0bba0063aa62af670fb28810
collins <kevin1520smart@gmail.com>
2023-06-08 22:05:28 +0100

    modified icon.js

--- src/data/icon.js
+++ src/data/icon.js
@@ -3,10 +3,12 @@ import { EnvelopeSimple, Phone, Clock } from "phosphor-react";
 const icon = [
   {
     icon: EnvelopeSimple,
+    type: "email",
     txt: "contact@logistics.com",
   },
   {
     icon: Phone,
+    type: "call us",
     txt: "(00) 112 365 489",
   },
   {
