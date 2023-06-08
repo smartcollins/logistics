034f66f8ff136afbf9a4b616882e9c50a40d6af7
collins <kevin1520smart@gmail.com>
2023-06-08 21:48:42 +0100

    modifying Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,11 @@ const Home = ()=>{
             <Stat/>
             <div style={{backgroundColor: color.blueBg}}>
                 <div>
-                    <Input/>
+                    <div className=' space-y-4 md:space-y-0 md:grid grid-rows-4 grid-cols-2 gap-4 font-["League_Spartan"] capitalize text-4xl'>
+                        <Input/>
+                        <textarea placeholder='Your Message' className=' indent-4 placeholder:py-4 text-2xl lg:placeholder:text-2xl placeholder:text-lg placeholder:text-white/80 placeholder:text-white bg-transparent border border-white w-full col-span-2 row-span-2' name="" id=""></textarea>
+                    </div>
+                    <button style={{ color: color.darkTxt, background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` }} className='font-medium capitalize text-center mt-8 py-2 w-full md:text-xl md:py-4 md:w-2/5'>submit message</button>
                 </div>
             </div>
             <div className=' space-y-4 mx-4 lg:my-8 lg:w-3/4 lg:mx-auto xl:w-3/5'>
