50c8f3ad456c54f96745c45b068fab83fbe3a51d
collins <kevin1520smart@gmail.com>
2023-06-08 22:25:56 +0100

    modifying Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,15 +20,23 @@ const Home = ()=>{
             <div>{banner.slice(0, 1).map((item, idx) => <Banner key={idx} {...item} />)}</div>
             <Stat/>
             <div style={{backgroundColor: color.blueBg}}>
-                <div>
-                    <div className='bg-red-300 space-y-3'>
-                        <Icon alt={true}/>
+                <div className='flex gap-8 w-3/4 mx-auto'>
+                    <div className='text-white w-1/3 space-y-4'>
+                        <div>
+                            <Caption txt={"contact"} txt2={"get in touch with us"}/>
+                            <p className='-mt-4'>Leverage agile frameworks to provide a robust synopsis for strategy foster collaborative thinking to further the overall value.</p>
+                        </div>
+                        <div className='space-y-3'>
+                            <Icon alt={true}/>
+                        </div>
                     </div>
-                    <div className=' space-y-4 md:space-y-0 md:grid grid-rows-4 grid-cols-2 gap-4 font-["League_Spartan"] capitalize text-4xl'>
-                        <Input/>
-                        <textarea placeholder='Your Message' className=' indent-4 placeholder:py-4 text-2xl lg:placeholder:text-2xl placeholder:text-lg placeholder:text-white/80 placeholder:text-white bg-transparent border border-white w-full col-span-2 row-span-2' name="" id=""></textarea>
+                    <div>
+                        <div className=' space-y-4 md:space-y-0 md:grid grid-rows-4 grid-cols-2 gap-4 font-["League_Spartan"] capitalize text-4xl'>
+                            <Input/>
+                            <textarea placeholder='Your Message' className=' indent-4 placeholder:py-4 text-2xl lg:placeholder:text-2xl placeholder:text-lg placeholder:text-white/80 placeholder:text-white bg-transparent border border-white w-full col-span-2 row-span-2' name="" id=""></textarea>
+                        </div>
+                        <button style={{ color: color.darkTxt, background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` }} className='font-medium capitalize text-center mt-8 py-2 w-full md:text-xl md:py-4 md:w-2/5'>submit message</button>
                     </div>
-                    <button style={{ color: color.darkTxt, background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` }} className='font-medium capitalize text-center mt-8 py-2 w-full md:text-xl md:py-4 md:w-2/5'>submit message</button>
                 </div>
             </div>
             <div className=' space-y-4 mx-4 lg:my-8 lg:w-3/4 lg:mx-auto xl:w-3/5'>
