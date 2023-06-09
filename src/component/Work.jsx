e034dda7297c4489cb99fe286c08d8ac23a69395
collins <kevin1520smart@gmail.com>
2023-06-09 10:12:33 +0100

    still modifying Work

--- src/component/Work.jsx
+++ src/component/Work.jsx
@@ -5,7 +5,8 @@ import work from "../data/work";
 const Work = ({icon,txt}) => {
   return (
     <div className="space-y-8">
-      <div className=" space-y-4 gap-8 md:space-y-0 md:grid md:grid-cols-2 lg:grid-cols-3 mx-auto">
+      {/* <div className=" space-y-4 gap-8 md:space-y-0 md:grid md:grid-cols-2 lg:grid-cols-3 mx-auto"> */}
+      <div>
         {/* {work.map((item, idx) => ( */}
           {/* <div key={idx} className="group md:text-left"> */}
           <div className="group md:text-left">
@@ -17,7 +18,7 @@ const Work = ({icon,txt}) => {
           </div>
         {/* ))} */}
       </div>
-      <button style={{ background: color.blueBg }} className='text-white py-2 px-8 rounded'>More Works</button>
+      {/* <button style={{ background: color.blueBg }} className='text-white py-2 px-8 rounded'>More Works</button> */}
     </div>
   );
 };
