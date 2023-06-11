2355a218c6f384e432a38875bc787987126c206b
collins <kevin1520smart@gmail.com>
2023-06-11 17:29:47 +0100

    formatted News

--- src/component/News.jsx
+++ src/component/News.jsx
@@ -3,15 +3,12 @@ import cal from "../icons/cal-Icon.svg";
 import color from "../data/color";
 import news from "../data/news";
 
-const News = ({start,end}) => {
-  return (
-    news.slice(start,end).map((item,idx)=>
+const News = ({ start, end }) => {
+  return news.slice(start, end).map((item, idx) => (
     <div key={idx} className="lg:flex gap-4 border-y py-4 group">
       <div className=" lg:w-3/4 relative w-full">
-        <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-5/6 w-5/6 hidden bg-[#1c1f35]/80 group-hover:flex items-center justify-center font-bold text-white">
-          Read more
-        </p>
-        <img className="h-full w-full object-cover md:h-[40vh] lg:h-full" src={item.img} alt="news" />
+        <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-5/6 w-5/6 hidden bg-[#1c1f35]/80 group-hover:flex items-center justify-center font-bold text-white">Read more</p>
+        <img className="h-full w-full object-cover md:h-[40vh] lg:h-full" src={item.img} alt="news"/>
       </div>
       <div className=" text-center flex items-center justify-center my-4 gap-4 lg:block lg:my-0">
         <img src={cal} alt="calender" />
@@ -19,10 +16,8 @@ const News = ({start,end}) => {
           <p style={{ color: color.darkTxt }} className=" font-bold text-2xl">
             {item.date}
           </p>
-          <p style={{ color: color.grayTxt }} className=" capitalize text-sm">
-            march
-          </p>
-        </div>        
+          <p style={{ color: color.grayTxt }} className=" capitalize text-sm">march</p>
+        </div>
       </div>
       <div className='font-["Rubik"] border-l border-[#d6d6d6] px-4'>
         <p className="font-bold text-[#1c1f35] text-lg group-hover:text-[#ffbe34] capitalize">
@@ -37,9 +32,8 @@ const News = ({start,end}) => {
           ))}
         </ul>
       </div>
-    </div>)
-    
-  );
+    </div>
+  ));
 };
 
 export default News;
