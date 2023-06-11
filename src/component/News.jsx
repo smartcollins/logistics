ea25efab4b6613d6abe9488cad87e5df0d0fd320
collins <kevin1520smart@gmail.com>
2023-06-11 17:27:59 +0100

    modified News

--- src/component/News.jsx
+++ src/component/News.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import cal from "../icons/cal-Icon.svg";
 import color from "../data/color";
+import news from "../data/news";
 
-const News = ({ img, date, info, txt, list }) => {
+const News = ({start,end}) => {
   return (
-    <div className="lg:flex gap-4 border-y py-4 group">
+    news.slice(start,end).map((item,idx)=>
+    <div key={idx} className="lg:flex gap-4 border-y py-4 group">
       <div className=" lg:w-3/4 relative w-full">
         <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-5/6 w-5/6 hidden bg-[#1c1f35]/80 group-hover:flex items-center justify-center font-bold text-white">
           Read more
         </p>
-        <img className="h-full w-full object-cover md:h-[40vh] lg:h-full" src={img} alt="news" />
+        <img className="h-full w-full object-cover md:h-[40vh] lg:h-full" src={item.img} alt="news" />
       </div>
       <div className=" text-center flex items-center justify-center my-4 gap-4 lg:block lg:my-0">
         <img src={cal} alt="calender" />
         <div>
           <p style={{ color: color.darkTxt }} className=" font-bold text-2xl">
-            {date}
+            {item.date}
           </p>
           <p style={{ color: color.grayTxt }} className=" capitalize text-sm">
             march
@@ -24,18 +26,19 @@ const News = ({ img, date, info, txt, list }) => {
       </div>
       <div className='font-["Rubik"] border-l border-[#d6d6d6] px-4'>
         <p className="font-bold text-[#1c1f35] text-lg group-hover:text-[#ffbe34] capitalize">
-          {info}
+          {item.info}
         </p>
-        <p className="text-[#666c89]">{txt}</p>
+        <p className="text-[#666c89]">{item.txt}</p>
         <ul className=" list-inside list-disc">
-          {list.map((itm, idx) => (
+          {item.list.map((itm, idx) => (
             <li key={idx} className=" first-letter:capitalize">
               {itm}
             </li>
           ))}
         </ul>
       </div>
-    </div>
+    </div>)
+    
   );
 };
 
