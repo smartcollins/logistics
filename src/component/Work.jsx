7dc36d94a2a828289eb49b06f0758ea0a17d7925
collins <kevin1520smart@gmail.com>
2023-06-13 09:05:22 +0100

    modified Work

--- src/component/Work.jsx
+++ src/component/Work.jsx
@@ -2,26 +2,27 @@ import React from "react";
 import color from "../data/color";
 import work from "../data/work";
 
-const Work = ({ start, end }) => {
+const Work = ({ start, end,home}) => {
   return (
     <div className="space-y-8">
-      <div className=" space-y-4 gap-8 md:space-y-0 md:grid md:grid-cols-2 lg:grid-cols-3 mx-auto">
+      <div className= {home?"w-full":"space-y-4 gap-8 md:space-y-0 md:grid md:grid-cols-2 lg:grid-cols-3 mx-auto"}>
         {work.slice(start, end).map((item, idx) => (
-          <div key={idx} className="group md:text-left">
-            <img className="mx-auto md:m-0" src={item.icon} alt="icon-logo" />
-            <p className=" text-[#1c1f35] group-hover:text-[#ffb82b] text-2xl capitalize">
-              {item.txt}
-            </p>
-            <p style={{ color: color.grayTxt }}>following the quality of our service thus having gained trust of our many clients</p>
+          <div key={idx} className={home?"group md:text-left flex gap-4 items-start bg-red-300":"group md:text-left bg-red-300"}>
+            <div className={home?"w-2/5 bg-red-400":undefined}>
+              <img className={home?"mx-auto object-contain bg-red-500":"mx-auto md:m-0"} src={item.icon} alt="icon-logo" />
+            </div>
+            <div className={home?" border-l px-4":undefined}>
+              <p className=" text-[#1c1f35] group-hover:text-[#ffb82b] text-2xl capitalize">
+                {item.txt}
+              </p>
+              <p style={{ color: color.grayTxt }}>following the quality of our service thus having gained trust of our many clients</p>
+            </div>
           </div>
         ))}
       </div>
-      <button
-        style={{ background: color.blueBg }}
-        className="text-white py-2 px-8 rounded"
-      >
+      {home?undefined:<button style={{ background: color.blueBg }} className="text-white py-2 px-8 rounded">
         More Works
-      </button>
+      </button>}
     </div>
   );
 };
