1be4b0ec17b201fc95dbc7643519fcbfc0fd845e
collins <kevin1520smart@gmail.com>
2023-06-16 22:58:54 +0100

    formatted fact.js

--- src/data/fact.js
+++ src/data/fact.js
@@ -1,14 +1,15 @@
-import cube from '../icons/cube-icon.svg'
-import ship from '../icons/ship-icon.svg'
-import globe from '../icons/globe-icon.svg'
-import call from '../icons/call-icon.svg'
-import clock from '../icons/clock-icon.svg'
-import drum from '../icons/drum-icon.svg'
+import cube from "../icons/cube-icon.svg";
+import ship from "../icons/ship-icon.svg";
+import globe from "../icons/globe-icon.svg";
+import call from "../icons/call-icon.svg";
+import clock from "../icons/clock-icon.svg";
+import drum from "../icons/drum-icon.svg";
 const fact = [
-    {icon: cube,txt:'safe package'},
-    {icon: ship,txt:'ship everywhere'},
-    {icon: globe,txt:'global tracking'},
-    {icon: call,txt:'24/7 support'},
-    {icon: clock,txt:'in time delivery'},
-    {icon: drum,txt:'transparent pricing'}
-]
+  { icon: cube, txt: "safe package" },
+  { icon: ship, txt: "ship everywhere" },
+  { icon: globe, txt: "global tracking" },
+  { icon: call, txt: "24/7 support" },
+  { icon: clock, txt: "in time delivery" },
+  { icon: drum, txt: "transparent pricing" },
+];
+export default fact;
