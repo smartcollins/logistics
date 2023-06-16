ce04454138b131e56ad8ee597755cb62b8b940cd
collins <kevin1520smart@gmail.com>
2023-06-16 22:58:08 +0100

    added icons to fact.js

--- src/data/fact.js
+++ src/data/fact.js
@@ -1,3 +1,9 @@
+import cube from '../icons/cube-icon.svg'
+import ship from '../icons/ship-icon.svg'
+import globe from '../icons/globe-icon.svg'
+import call from '../icons/call-icon.svg'
+import clock from '../icons/clock-icon.svg'
+import drum from '../icons/drum-icon.svg'
 const fact = [
     {icon: cube,txt:'safe package'},
     {icon: ship,txt:'ship everywhere'},
