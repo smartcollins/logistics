372c40c5969f3ff2ae54edf120e313a303084754
collins <kevin1520smart@gmail.com>
2023-06-16 22:57:28 +0100

    created fact.js

---
+++ src/data/fact.js
@@ -0,0 +1,8 @@
+const fact = [
+    {icon: cube,txt:'safe package'},
+    {icon: ship,txt:'ship everywhere'},
+    {icon: globe,txt:'global tracking'},
+    {icon: call,txt:'24/7 support'},
+    {icon: clock,txt:'in time delivery'},
+    {icon: drum,txt:'transparent pricing'}
+]
