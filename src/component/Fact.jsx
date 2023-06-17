abbe2eb9d02875a1fd5569911a5a7d8cdc7bb12f
collins <kevin1520smart@gmail.com>
2023-06-17 22:40:56 +0100

    minor modification in Fact

--- src/component/Fact.jsx
+++ src/component/Fact.jsx
@@ -5,7 +5,7 @@ import color from '../data/color';
 const Fact = ({icon,txt})=>{
     return(
         <div className='flex items-center gap-4'>
-            <div style={{background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)`}} className=' rounded-full w-16 h-16 p-4'>
+            <div style={{background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)`}} className=' rounded-full w-12 h-12 p-2'>
                 <img src={icon} alt="icon" />
             </div>
             <p className=' capitalize text-lg'>{txt}</p>
