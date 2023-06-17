55fddbfca97829ed687e7eb0c1773b08b52f923d
collins <kevin1520smart@gmail.com>
2023-06-17 23:19:36 +0100

    modifying Trans

--- src/component/Trans.jsx
+++ src/component/Trans.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import tran from '../images/tran.png'
 
 
 const Trans = ()=>{
     return(
-        <div></div>
+        <div>
+            <div className=' bg-blue-500 relative object-contain'>
+                <img className='h-full object-cover' src={tran} alt="ideas" />
+                {/* <div className=' text-red-300'> */}
+                    <p className='absolute'>specialized transport</p>
+                    <p>ocean transports</p>
+                {/* </div> */}
+            </div>
+            {/* <div style={{backgroundImage: `url(${tran})`}}>
+                <p>specialized transport</p>
+                <p>ocean transports</p>
+            </div> */}
+        </div>
     )
 }
 
