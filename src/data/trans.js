d3d307ae8e2ae0cdad25f47854f4f2e0f61f0fc6
collins <kevin1520smart@gmail.com>
2023-06-18 16:58:44 +0100

    created trans.js

---
+++ src/data/trans.js
@@ -0,0 +1,29 @@
+const trans = [
+    {
+        img: tran,
+        txt: 'liquid transportation',
+        txt2: 'premium tankers'
+    },
+    {
+        img: tran2,
+        txt: 'packaging solutions',
+        txt2: 'warehouse management'
+    },
+    {
+        img: tran3,
+        txt: 'ontract logistics',
+        txt2: 'road transportation'
+    },
+    {
+        img: tran4,
+        txt: 'warehouse & distribution',
+        txt2: 'large warehouse'
+    },
+    {
+        img: tran5,
+        txt: 'specialized transport',
+        txt2: 'ocean transport'
+    }
+]
+
+export default trans;
