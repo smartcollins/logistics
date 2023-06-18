c38caa4c3f62ccb52e8233820aba90e7927bf1a4
collins <kevin1520smart@gmail.com>
2023-06-18 17:02:20 +0100

    added images to trans.js

--- src/data/trans.js
+++ src/data/trans.js
@@ -1,3 +1,10 @@
+import tran from '../images/tran.png'
+import tran2 from '../images/tran2.png'
+import tran3 from '../images/tran3.png'
+import tran4 from '../images/tran4.png'
+import tran5 from '../images/tran5.png'
+
+
 const trans = [
     {
         img: tran,
