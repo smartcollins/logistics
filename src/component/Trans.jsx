549e77815d113e6c97322df265518002860db04e
collins <kevin1520smart@gmail.com>
2023-06-18 16:43:02 +0100

    formatted Trans

--- src/component/Trans.jsx
+++ src/component/Trans.jsx
@@ -1,17 +1,14 @@
-import React from 'react';
+import React from "react";
 
-
-
-const Trans = ({img,txt,txt2})=>{
-    return(
-        <div style={{backgroundImage: `url(${img})`}} className='relative h-[60vh] px-4 bg-no-repeat'>
-            <div className='absolute bottom-4 capitalize'>
-                <p className='text-2xl font-semibold text-white'>{txt}</p>
-                <p className='text-[#ffb629] text-lg font-medium'>{txt2}</p>
-            </div>
-        </div>
-    )
-}
-
+const Trans = ({ img, txt, txt2 }) => {
+  return (
+    <div style={{ backgroundImage: `url(${img})` }} className="relative h-[60vh] px-4 bg-no-repeat">
+      <div className="absolute bottom-4 capitalize">
+        <p className="text-2xl font-semibold text-white">{txt}</p>
+        <p className="text-[#ffb629] text-lg font-medium">{txt2}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Trans;
