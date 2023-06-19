674bd9349bd795427a6a741b624cd054ab9cfaf8
collins <kevin1520smart@gmail.com>
2023-06-19 23:04:33 +0100

    formatted Fact

--- src/component/Fact.jsx
+++ src/component/Fact.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import color from '../data/color';
 
 
-const Fact = ({icon,txt})=>{
-    return(
+const Fact = ({ icon, txt }) => {
+    return (
         <div className='flex items-center gap-4'>
             {/* <div style={{background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)`}} className=' rounded-full w-12 h-12 p-2'> */}
             {/* <div style={{background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)`}} className=' rounded-full w-14 h-14 p-4'> */}
             {/* <div style={{background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)`}} className=' rounded-full w-14 h-14 p-4'> */}
-            <div style={{background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)`}} className=' rounded-full w-12 h-12 p-2 flex items-center justify-center'>
+            <div style={{ background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)` }} className=' rounded-full w-12 h-12 p-2 flex items-center justify-center'>
                 <img src={icon} alt="icon" />
             </div>
             <p className=' capitalize text-lg'>{txt}</p>
