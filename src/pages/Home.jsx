84c8bce938935dd1458f0c9c9dfd8dd0f61cab50
collins <kevin1520smart@gmail.com>
2023-06-24 14:21:23 +0100

    cleaned up Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -81,7 +81,6 @@ const Home = ()=>{
                 <Review />
             </div>
             <div className='lg:flex pb-4 xl:pb-auto gap-12 text-[#1c1f35] xl:py-20 bg-gradient-to-b xl:bg-gradient-to-r from-[#091242] xl:from-[50%] to-[#f4f4f4] to-[50%]'>
-                {/* <div className='bg-[#091242]'> */}
                     <div className='bg-red-400 relative h-4/5 md:h-[50vh] lg:w-full xl:h-[90%] overflow-hidden xl:ml-24'>
                         <img className='h-full w-full object-cover' src={side2} alt="bg-img" />
                         <div style={{background: 'linear-gradient(94.06deg, #FFB629 -1.21%, #FFDA56 58.66%, #FFD7A6 116.84%)'}} className=' lg:absolute lg:w-4/5 lg:left-1/2 lg:-translate-x-1/2 bottom-0 flex items-center capitalize text-lg font-medium gap-4 py-2 px-4'>
@@ -89,7 +88,6 @@ const Home = ()=>{
                             <p>moving your product across borders</p>
                         </div>
                     </div>
-                {/* </div> */}
                 <div className=' space-y-8 px-4 lg:px-0 lg:space-y-4 xl:space-y-0 '>
                     <div className='-space-y-4'>
                         <Caption txt={'why choose'} txt2={'we create opportunity to reach potential'}/>
@@ -97,7 +95,6 @@ const Home = ()=>{
                     </div>
                     <div className='space-y-4 md:space-y-0 md:grid grid-cols-2 gap-4'>
                         {facts.map((item,idx)=><div key={idx} className=''><Fact {...item}/></div>)}
-                        {/* {facts.map((item,idx)=><Fact key={idx} {...item}/>)} */}
                     </div>
                 </div>
             </div>
