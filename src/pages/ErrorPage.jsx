067037badf3a702d5ad4292f058fd6f47d488f6c
collins <kevin1520smart@gmail.com>
2023-06-27 23:13:02 +0100

    minor format made in ErrorPage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -13,12 +13,11 @@ const ErrorPage = () => {
                 <p className='md:text-[200px] text-9xl font-semibold'>4<span className='text-[#ffbb2b]'>0</span>4</p>
                 <p className='font-bold text-2xl'>Oops! Page not found.</p>
                 <p className='text-[#666c89] text-base tracking-tighter'>Lets's get you to where you need to be.</p>
-                <button style={{
-                    background: `linear-gradient(94.06deg,
-                                ${color.grdt} -1.21%, ${color.grdt2} 58.66%,
-                                ${color.grdt3} 116.84%)`
-                }}
-                    className='px-4 py-2 my-4 font-medium text-black'>Back to home</button>
+                <button
+                    style={{background: `linear-gradient(94.06deg,${color.grdt} -1.21%, ${color.grdt2} 58.66%,${color.grdt3} 116.84%)`}}
+                    className='px-4 py-2 my-4 font-medium text-black'>
+                    Back to home
+                </button>
             </div>
             <Footer />
         </div>
