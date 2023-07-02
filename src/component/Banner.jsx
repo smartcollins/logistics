5a0104e458cb44082645f2aeb9e32465f2aef46d
collins <kevin1520smart@gmail.com>
2023-07-02 23:03:17 +0100

    removed blog prop from Banner

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -4,7 +4,7 @@ import icon2 from "../icons/cal-w-Icon.svg";
 import color from "../data/color";
 import { TwitterLogo, FacebookLogo, LinkedinLogo, InstagramLogo, } from "phosphor-react";
 
-const Banner = ({ img, txt, txt2, blog, extra, info, btn_txt }) => {
+const Banner = ({ img, txt, txt2, extra, info, btn_txt }) => {
   const blogGrdt = {
     background: `linear-gradient(91.25deg, rgba(3, 45, 95, 0.54) 9.39%, rgba(9, 18, 95, 0.49) 93.62%),
     url(${img})`,
@@ -13,7 +13,7 @@ const Banner = ({ img, txt, txt2, blog, extra, info, btn_txt }) => {
   };
   return (
     <div className="h-[80vh] bg-red-200 text-white capitalize">
-      <div style={blog ? blogGrdt : { backgroundImage: `url(${img})` }} className="bg-no-repeat bg-cover bg-center h-full relative">
+      <div style={{ backgroundImage: `url(${img})` }} className="bg-no-repeat bg-cover bg-center h-full relative">
         <div className="lg:w-3/4 lg:m-auto">
           <div className=" bg-[#091242]/5 font-medium flex items-center justify-between">
             <div className="flex items-center text-white w-full text-center lg:w-3/5 lg:-ml-5 xl:-ml-9">
@@ -35,21 +35,6 @@ const Banner = ({ img, txt, txt2, blog, extra, info, btn_txt }) => {
               </button>
             </div>
           </div>
-          {blog ? (
-            <div className=" absolute top-1/2 -translate-y-1/2">
-              <div className="flex items-center gap-2 w-fit">
-                <img className=" w-10 h-full" src={icon2} alt="calender-logo" />
-                <div className="flex flex-col capitalize text-lg">
-                  <p className="text-2xl font-bold">12</p>
-                  <p>march</p>
-                </div>
-              </div>
-              <p className="font-[Rubik] font-semibold text-4xl/normal">
-                {info}
-              </p>
-            </div>
-          ) : (
-            // <div className=" absolute top-1/2 -translate-y-1/2 w-full md:w-2/4">
             <div className=" absolute top-1/2 -translate-y-1/2 w-full mx-4 md:mx-8 lg:mx-0 md:w-2/4 lg:w-2/3 xl:w-2/4">
               <p style={{ borderLeft: `2px solid ${color.grdt}` }} className="bg-[#041c37]/50 w-fit px-2 py-1">
                 {txt}
@@ -71,7 +56,6 @@ const Banner = ({ img, txt, txt2, blog, extra, info, btn_txt }) => {
                 </div>
               )}
             </div>
-          )}
         </div>
       </div>
     </div>
