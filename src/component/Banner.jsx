1d6b56e2bfe00b18b73254eb8273214c2fb94eed
collins <kevin1520smart@gmail.com>
2023-07-02 23:04:39 +0100

    formmated Banner

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -1,21 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import color from "../data/color";
-import { TwitterLogo, FacebookLogo, LinkedinLogo, InstagramLogo, } from "phosphor-react";
+import {
+  TwitterLogo,
+  FacebookLogo,
+  LinkedinLogo,
+  InstagramLogo,
+} from "phosphor-react";
 
 const Banner = ({ img, txt, txt2, extra, info, btn_txt }) => {
   return (
     <div className="h-[80vh] bg-red-200 text-white capitalize">
-      <div style={{ backgroundImage: `url(${img})` }} className="bg-no-repeat bg-cover bg-center h-full relative">
+      <div
+        style={{ backgroundImage: `url(${img})` }}
+        className="bg-no-repeat bg-cover bg-center h-full relative"
+      >
         <div className="lg:w-3/4 lg:m-auto">
           <div className=" bg-[#091242]/5 font-medium flex items-center justify-between">
             <div className="flex items-center text-white w-full text-center lg:w-3/5 lg:-ml-5 xl:-ml-9">
-              <Link className="border-r last:border-none border-r-white w-full" to={'/'}>home</Link>
-              <Link className="border-r last:border-none border-r-white w-full" to={'/about'}>about</Link>
-              <Link className="border-r last:border-none border-r-white w-full" to={'/'}>pages</Link>
-              <Link className="border-r last:border-none border-r-white w-full" to={'/project'}>projects</Link>
-              <Link className="border-r last:border-none border-r-white w-full" to={'/contact'}>contact</Link>
-            </div>            
+              <Link
+                className="border-r last:border-none border-r-white w-full"
+                to={"/"}
+              >
+                home
+              </Link>
+              <Link
+                className="border-r last:border-none border-r-white w-full"
+                to={"/about"}
+              >
+                about
+              </Link>
+              <Link
+                className="border-r last:border-none border-r-white w-full"
+                to={"/"}
+              >
+                pages
+              </Link>
+              <Link
+                className="border-r last:border-none border-r-white w-full"
+                to={"/project"}
+              >
+                projects
+              </Link>
+              <Link
+                className="border-r last:border-none border-r-white w-full"
+                to={"/contact"}
+              >
+                contact
+              </Link>
+            </div>
             <div className=" hidden md:flex items-center justify-between w-2/5 bg-blue-500">
               <div className="flex items-center justify-between w-2/5 md:w-full lg:w-2/5 bg-red-400">
                 <InstagramLogo size={20} color="#ffffff" />
@@ -28,27 +61,30 @@ const Banner = ({ img, txt, txt2, extra, info, btn_txt }) => {
               </button>
             </div>
           </div>
-            <div className=" absolute top-1/2 -translate-y-1/2 w-full mx-4 md:mx-8 lg:mx-0 md:w-2/4 lg:w-2/3 xl:w-2/4">
-              <p style={{ borderLeft: `2px solid ${color.grdt}` }} className="bg-[#041c37]/50 w-fit px-2 py-1">
-                {txt}
-              </p>
-              <p className="font-[Rubik] font-semibold text-4xl/normal w-3/4">
-                {info}
-              </p>
-              {extra && (
-                <div>
-                  <p className=" font-[Krub] font-medium w-3/5 my-2 ">{txt2}</p>
-                  <button
-                    style={{
-                      background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)`,
-                    }}
-                    className="p-2 text-black capitalize font-medium"
-                  >
-                    {btn_txt}
-                  </button>
-                </div>
-              )}
-            </div>
+          <div className=" absolute top-1/2 -translate-y-1/2 w-full mx-4 md:mx-8 lg:mx-0 md:w-2/4 lg:w-2/3 xl:w-2/4">
+            <p
+              style={{ borderLeft: `2px solid ${color.grdt}` }}
+              className="bg-[#041c37]/50 w-fit px-2 py-1"
+            >
+              {txt}
+            </p>
+            <p className="font-[Rubik] font-semibold text-4xl/normal w-3/4">
+              {info}
+            </p>
+            {extra && (
+              <div>
+                <p className=" font-[Krub] font-medium w-3/5 my-2 ">{txt2}</p>
+                <button
+                  style={{
+                    background: `linear-gradient(94.06deg, ${color.grdt} -1.21%, ${color.grdt2} 58.66%, ${color.grdt3} 116.84%)`,
+                  }}
+                  className="p-2 text-black capitalize font-medium"
+                >
+                  {btn_txt}
+                </button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
