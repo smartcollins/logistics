8676d8f0510375bd00374b525eeb2fecf94c0ba0
collins <kevin1520smart@gmail.com>
2023-07-07 00:16:39 +0100

    formatted Layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
-import Footer from './component/Footer';
-import Banner from './component/Banner';
-import banner from './data/banner';
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Footer from "./component/Footer";
+import Banner from "./component/Banner";
+import banner from "./data/banner";
 
+const Layout = () => {
+  const location = useLocation();
+  const recent = banner.filter((item) => `/${item.path}` === location.pathname);
+  return (
+    <div>
+      {recent.map((itm, idx) => (
+        <Banner key={idx} {...itm} />
+      ))}
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
-const Layout = ()=>{
-    const location = useLocation();
-    const recent = banner.filter(item => `/${item.path}` === location.pathname)
-    return(
-        <div>
-            {recent.map((itm,idx)=><Banner key={idx} {...itm}/>)}
-            <Outlet/>
-            <Footer/>
-        </div>
-    )
-}
-
-
-export default Layout; 
+export default Layout;
