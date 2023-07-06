d637747435ec8480a25fd78780595646a127dae4
collins <kevin1520smart@gmail.com>
2023-07-06 23:59:05 +0100

    modifying Layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Footer from './component/Footer';
+import Banner from './component/Banner';
+import banner from './data/banner';
 
 
 const Layout = ()=>{
+    const location = useLocation();
+    const loc = location.pathname;
+    // const me = banner.filter(item => item.img)
+    // const recent = banner.filter(item => `/${item.img}` === location.pathname)
+    // const recent = banner.filter(item => item.img === 'home')
+    const recent = banner.filter(item => `/${item.path}` === location.pathname)
+
+    console.log(loc)
+    console.log(location)
+    console.log(recent)
     return(
         <div>
+            {/* {banner.filter((item.img === location))} */}
             <Outlet/>
             <Footer/>
         </div>
