c47c9508e406ecb02ce7d2e245bf78740af7c397
collins <kevin1520smart@gmail.com>
2023-07-07 00:06:33 +0100

    cleaned up Layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,18 +7,10 @@ import banner from './data/banner';
 
 const Layout = ()=>{
     const location = useLocation();
-    const loc = location.pathname;
-    // const me = banner.filter(item => item.img)
-    // const recent = banner.filter(item => `/${item.img}` === location.pathname)
-    // const recent = banner.filter(item => item.img === 'home')
     const recent = banner.filter(item => `/${item.path}` === location.pathname)
-
-    console.log(loc)
-    console.log(location)
-    console.log(recent)
     return(
         <div>
-            {/* {banner.filter((item.img === location))} */}
+            {recent.map((itm,idx)=><Banner key={idx} {...itm}/>)}
             <Outlet/>
             <Footer/>
         </div>
