67afd7e0db4a07ae4d0d83918800f7e9d320a90d
collins <kevin1520smart@gmail.com>
2023-07-06 22:28:56 +0100

    added Outlet to Layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Footer from './component/Footer';
 
 
 const Layout = ()=>{
     return(
         <div>
+            <Outlet/>
             <Footer/>
         </div>
     )
