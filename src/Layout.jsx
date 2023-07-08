d50005079fe767b2d4a3766f7e139bb22a89390a
collins <kevin1520smart@gmail.com>
2023-07-08 20:55:49 +0100

    modified Layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
+import banner from "./data/banner";
 import Footer from "./component/Footer";
 import Banner from "./component/Banner";
-import banner from "./data/banner";
 
 const Layout = () => {
   const location = useLocation();
-  const recent = banner.filter((item) => `/${item.path}` === location.pathname);
+  const recent = banner.filter((item) => {
+    console.log(item)
+    return (`/${item.path}` === location.pathname) || (item.path === 'home' && location.pathname === '/')
+});
+  console.log(location)
+  
+  console.log(recent)
   return (
     <div>
+
+      {/* {
+        recent === [] ? <div>
+            {banner.slice(0,1).map((itm, idx) => (
+        <Banner key={idx} {...itm} />
+      ))}
+        </div>:<div>
+            {recent.map((itm, idx) => (
+                <Banner key={idx} {...itm} />
+            ))}
+        </div>
+      } */}
+
       {recent.map((itm, idx) => (
         <Banner key={idx} {...itm} />
       ))}
