cd5ed545a67512f36fefebd74953d4c2c0b9b123
collins <kevin1520smart@gmail.com>
2023-07-09 14:56:04 +0100

    cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-// Issues
-// 1. can't place a component above routeProvider.
-// 2. can't place a link tag as a child in either select or option tag.
-
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
 import Home from "./pages/Home";
-import Nav from "./component/Nav";
-import Footer from "./component/Footer";
 import ErrorPage from "./pages/ErrorPage";
 import About from "./pages/About";
 import Service from "./pages/Service";
@@ -24,83 +17,6 @@ import Protection from "./pages/Protection";
 import Layout from "./Layout";
 
 function App() {
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <Home />,
-  //     errorElement: <ErrorPage />,
-  //   },
-  //   {
-  //     path: "/nav",
-  //     element: <Nav />,
-  //     errorElement: <ErrorPage />,
-  //   },
-  //   {
-  //     path: "/about",
-  //     element: <About />,
-  //     errorElement: <ErrorPage />,
-  //   },
-  //   {
-  //     path: "/service",
-  //     element: <Service />,
-  //     errorElement: <ErrorPage />,
-  //   },
-  //   {
-  //     path: "/service-info",
-  //     element: <ServiceInfo />,
-  //     errorElement: <ErrorPage />,
-  //   },
-  //   {
-  //     path: "/project",
-  //     element: <Project />,
-  //     errorElement: <ErrorPage />,
-  //   },
-  //   {
-  //     path: "/project-info",
-  //     element: <ProjectInfo />,
-  //     errorElement: <ErrorPage />,
-  //   },
-  //   {
-  //     path: "/blog",
-  //     element: <Blog />,
-  //     errorElement: <ErrorPage />,
-  //   },
-  //   {
-  //     path: "/blog-info",
-  //     element: <BlogInfo />,
-  //     errorElement: <ErrorPage />,
-  //   },
-  //   {
-  //     path: "/team",
-  //     element: <Team />,
-  //     errorElement: <ErrorPage />,
-  //   },
-  //   {
-  //     path: "/contact",
-  //     element: <Contact />,
-  //     errorElement: <ErrorPage />,
-  //   },
-  //   {
-  //     path: "/pricing",
-  //     element: <Pricing />,
-  //     errorElement: <ErrorPage />,
-  //   },
-  //   {
-  //     path: "/change",
-  //     element: <Change />,
-  //     errorElement: <ErrorPage />,
-  //   },
-  //   {
-  //     path: "/liecenses",
-  //     element: <Liecenses />,
-  //     errorElement: <ErrorPage />,
-  //   },
-  //   {
-  //     path: "/protection",
-  //     element: <Protection />,
-  //     errorElement: <ErrorPage />,
-  //   },
-  // ]);
 
   const routes = createBrowserRouter([
     {
@@ -171,7 +87,6 @@ function App() {
 
   return (
     <div>
-      {/* <RouterProvider router={router} /> */}
       <RouterProvider router={routes} />
     </div>
   );
