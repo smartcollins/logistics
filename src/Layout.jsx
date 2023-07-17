c36520788fd49892ece8c1b5474fea228059e0f9
collins <kevin1520smart@gmail.com>
2023-07-17 22:26:11 +0100

    added Nav to Layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import banner from "./data/banner";
 import Banner from "./component/Banner";
+import Nav from "./component/Nav";
 import Footer from "./component/Footer";
 
 const Layout = () => {
@@ -9,6 +10,7 @@ const Layout = () => {
   const recent = banner.filter((item) => ((`/${item.path}` === location.pathname) || (item.path === 'home' && location.pathname === '/')))
   return (
     <div className="overflow-x-clip">
+      <Nav/>
       {recent.map((itm, idx) => (
         <Banner key={idx} {...itm} />
       ))}
