b9b619873038f67dafcf4f4a19d8df85d25633cb
collins <kevin1520smart@gmail.com>
2023-07-18 23:48:46 +0100

    minor modification made in Nav

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -6,10 +6,7 @@ import { Clock, Envelope, Phone } from "phosphor-react";
 const Nav = () => {
   return (
     <div className="text-white capitalize">
-      <div
-        style={{ backgroundColor: color.blueBg }}
-        className="flex items-center justify-between py-4"
-      >
+      <div style={{ backgroundColor: color.blueBg }} className="flex items-center justify-between py-4">
         <div className="mx-4 w-full lg:flex lg:w-3/4 lg:mx-auto">
           <div className="hidden xl:block w-1/3">
             <Logo />
