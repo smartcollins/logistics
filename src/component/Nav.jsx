6bb8a45b2598348adbef3d927e8637a775d671e4
collins <kevin1520smart@gmail.com>
2023-07-20 22:18:54 +0100

    formatted Nav

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -4,35 +4,44 @@ import Logo from "./Logo";
 import { Clock, Envelope, Phone } from "phosphor-react";
 
 const Nav = () => {
-  const ary = [{
-    icon: Clock,
-    txt: 'mon - sat 9.00 - 18.00',
-    txt2: 'Sunday Closed'
-  },{
-    icon: Envelope,
-    txt: 'email',
-    txt2: 'contact@logistics.com'
-  },{
-    icon: Phone,
-    txt: 'call us',
-    txt2: '(00) 112 365 489'
-  }]
+  const ary = [
+    {
+      icon: Clock,
+      txt: "mon - sat 9.00 - 18.00",
+      txt2: "Sunday Closed",
+    },
+    {
+      icon: Envelope,
+      txt: "email",
+      txt2: "contact@logistics.com",
+    },
+    {
+      icon: Phone,
+      txt: "call us",
+      txt2: "(00) 112 365 489",
+    },
+  ];
 
-  const item = ary.map(({icon,txt,txt2},idx)=><div key={idx} className="flex items-center gap-4 w-full ">
-  <div className="p-3 rounded-full bg-[#111c55] border border-[#273270]">
-    {React.createElement(icon,{
-      size: 20,
-      color: "#f6b426"
-    })}
-  </div>
-  <div>
-    <p>{txt}</p>
-    <p>{txt2}</p>
-  </div>
-</div>)
+  const item = ary.map(({ icon, txt, txt2 }, idx) => (
+    <div key={idx} className="flex items-center gap-4 w-full ">
+      <div className="p-3 rounded-full bg-[#111c55] border border-[#273270]">
+        {React.createElement(icon, {
+          size: 20,
+          color: "#f6b426",
+        })}
+      </div>
+      <div>
+        <p>{txt}</p>
+        <p>{txt2}</p>
+      </div>
+    </div>
+  ));
   return (
     <div className="text-white capitalize hidden md:block">
-      <div style={{ backgroundColor: color.blueBg }} className="flex items-center justify-between py-4">
+      <div
+        style={{ backgroundColor: color.blueBg }}
+        className="flex items-center justify-between py-4"
+      >
         <div className="mx-4 w-full lg:flex lg:w-3/4 lg:mx-auto">
           <div className="hidden xl:block w-1/3">
             <Logo />
