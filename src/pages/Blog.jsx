c931f4bf9fcd772b552eab309fa122ade99c8fc5
collins <kevin1520smart@gmail.com>
2023-07-21 21:13:31 +0100

    removed overflow property from pages folder

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -5,7 +5,7 @@ import News from '../component/News';
 
 const Blog = () => {
     return (
-        <div className=' overflow-x-clip'>
+        <div>
             <Caption mid={true} dark={true} txt={'our blog'} txt2={'our latest news'} />
             <div className=' space-y-4 mx-4 lg:my-8 lg:w-3/4 lg:mx-auto xl:w-3/5'>
                 <News/>
